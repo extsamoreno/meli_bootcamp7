0d4829dbfcd0df0726a92f2a8a03e48957cfb396
Michael Andrey Rivera Castro <mirivera@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-05-26 11:19:17 -0500

    Adding Ejercicio5

--- 2_java1/src/Java1Practica.java
+++ 2_java1/src/Java1Practica.java
@@ -5,7 +5,7 @@ public class Java1Practica {
     public static Scanner input= new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+
     }
 
     public static void ejercicio1(){
@@ -38,6 +38,37 @@ public class Java1Practica {
         int n=input.nextInt();
         for (int i=0; i<=n; i++){
             if(isPrime(i)) System.out.print(i+" ");
+        }
+    }
+
+    public static void ejercicio5(){
+        System.out.print("Enter the value of n: ");
+        int n=input.nextInt();
+        System.out.print("Enter the value of m: ");
+        int m=input.nextInt();
+        System.out.print("Enter the value of d: ");
+        int d=input.nextInt();
+        int counter=1;
+        String tempInitialNumber="", tempActualNumber;
+        for (int i=0; i<m; i++){
+            tempInitialNumber+=d+"";
+        }
+        int actualNumber=Integer.parseInt(tempInitialNumber);
+        while (counter<=n){
+            tempActualNumber= actualNumber+"";
+            for (int i=0; i<m; i++){
+                if(tempActualNumber.indexOf(d+"")==-1){
+                    actualNumber++;
+                    break;
+                } else {
+                    tempActualNumber=tempActualNumber.replaceFirst(d+"","");
+                    if (i==m-1) {
+                        System.out.print(actualNumber+" ");
+                        counter++;
+                        actualNumber++;
+                    }
+                }
+            }
         }
     }
 
