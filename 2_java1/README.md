b2820226dee1e6f4a797489077fda6b9e2395f4d
Matias Eduardo Bossa Valverde <mbossa@ar0fvfcg18tl415.ml.com>
2021-05-26 13:25:56 -0300

    Excercise 2 Update

---
+++ 2_java1/README.md
@@ -0,0 +1,13 @@
+# LISTADO DE EJERCICIOS 
+
+## 1 - Numeros Pares 
+### Class - PairsNumbers
+
+## 2 - Numeros Divisibles
+### Class - DivisibleNumbers
+
+## 3 - Si es o no numero primo
+### Class - PrimeNumbers
+
+## 4 - Desde un nro N, listar cuales son primos o no
+### Class - N_PrimesNumbers
--- 2_java1/src/DivisiblesNumbers.java
+++ 2_java1/src/DivisiblesNumbers.java
@@ -4,14 +4,14 @@ public class DivisiblesNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese un numero entero N, que refleja de donde empezar");
-        int start = scanner.nextInt();
+        System.out.println("Ingrese un numero entero N, que refleja la cantidad de numeros a visualizar");
+        int quantity = scanner.nextInt();
 
-        System.out.println("Ingrese un numero entero M, que refleja de donde terminar");
-        int finish = scanner.nextInt();
+        System.out.println("Ingrese un numero entero M, que refleja el numero con el cual tienen que ser Divisible");
+        int divisibleNumber = scanner.nextInt();
 
-        for(int i = start; i <= finish; i++){
-            if(finish % i == 0 && i != 0) {
+        for(int i = 0; i <= quantity; i++){
+            if(i != 0 && divisibleNumber % i == 0) {
                 System.out.println(i);
             }
         }
