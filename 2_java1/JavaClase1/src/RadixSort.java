a767bb5d3e929c4ce37b3459823772de4fc728cd
Emmanuel Carron <diego.carron@mercadolibre.com>
2021-05-26 17:40:52 -0300

    StringUtils completed...

--- 2_java1/JavaClase1/src/RadixSort.java
+++ 2_java1/JavaClase1/src/RadixSort.java
@@ -1,7 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import functions.StringUtils;
+
+public class RadixSort
+{
+    public static void radixSort(int[] arr)
+    {
+        //Implementación
+        String[] strArray = StringUtils.toStringArray(arr);
+        StringUtils.lNormalize(strArray, '0');
+        HashMap<Integer, ArrayList<String>> myHash = new HashMap<>();
+
+        for (int i = 0; i < 10; i++) {
+            myHash.put(i, new ArrayList<>());
+        }
 
 
-public class RadixSort {
-    public static void main(String[] args) {
-        
+
+
+
+    }
+
+
+
+
+    public static void main(String[] args)
+    {
+        int[] arr={16223,898,13,906,235,23,9,1532,6388,2511,8};
+        radixSort(arr);
+
+        for(int i=0; i<arr.length;i++)
+        {
+            System.out.print(arr[i]+(i<arr.length-1?",":""));
+        }
     }
 }
+
--- 2_java1/JavaClase1/src/functions/Functions.java
+++ 2_java1/JavaClase1/src/functions/Functions.java
@@ -37,4 +37,5 @@ public class Functions {
         }
         return output;
     }
+
 }
