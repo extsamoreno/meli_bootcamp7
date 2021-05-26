8818136f9f641f768d373326e750c7158d12cdee
Agustin Nicolas Martinez <agustmartine@ar0c02ff0ktml85.ml.com>
2021-05-26 13:42:54 -0300

    clase 1 java numeros multiplos

---
+++ 2_java1/src/com/company/Act2.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+public class Act2 {
+    public static boolean isPrimno(int dataN, int dataM){
+        return dataN%dataM==0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese cantidad de multiplos ");
+        int valueN = sc.nextInt();
+        System.out.println("Ingrese numero que seran multiplos de el ");
+        int valueM = sc.nextInt();
+        for(int i=1; i<valueN; i++){
+            if (isPrimno(valueM,i)){
+                System.out.println(i);
+            }
+        }
+    }
+}
