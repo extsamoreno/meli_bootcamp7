e0c89da87a18f5996cae37db39fa0e0055aab51f
Antonella Mariel Lesta <alesta@ar0c02ff0l4ml85.ml.com>
2021-05-26 20:37:39 -0300

    tarde

---
+++ 2_java1/ClaseMañana/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Ejercicio numero 1");
+        Ejercicio1 ejercicio = new Ejercicio1();
+        ejercicio.Ejericio();
+        System.out.println("Ejercicio numero 2");
+        Ejercicio_2 ejercicio_2= new Ejercicio_2();
+        ejercicio_2.Ejercicio();
+        System.out.println("Punto 3");
+        Punto_3 pto3= new Punto_3();
+        pto3.Ejercicio();
+        System.out.println("Punto 4");
+        Punto_4 pto4= new Punto_4();
+        pto4.Ejercicio();
+        System.out.println("Punto 5");
+        Punto_5 pto5= new Punto_5();
+        pto5.Ejercicio();
+    }
+}
