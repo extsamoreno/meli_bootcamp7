587f5833aa17cf6450739b9fb581858ed15b8567
Michael Andrey Rivera Castro <mirivera@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-05-26 09:50:10 -0500

    Adding Ejercicio1

--- 2_java1/src/Java1Practica.java
+++ 2_java1/src/Java1Practica.java
@@ -1,6 +1,20 @@
-public class Java1Practica {
-    
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Java1Practica {
+
+    public static Scanner input= new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
     }
+
+    public static void ejercicio1(){
+        System.out.print("Enter the value of n: ");
+        int n= input.nextInt();
+        int actualPairNumber=0;
+        for (int i=0; i<n; i++){
+            System.out.print((actualPairNumber+=2)+" ");
+        }
+    }
+
 }
