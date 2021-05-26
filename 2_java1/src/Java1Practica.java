777641ca91dcb2f8d7da57e455ee99bbc2aa8d4a
Michael Andrey Rivera Castro <mirivera@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-05-26 10:49:22 -0500

    Finishing Ejercicio4 and Adding a comment to the isPrime method

--- 2_java1/src/Java1Practica.java
+++ 2_java1/src/Java1Practica.java
@@ -34,9 +34,21 @@ public class Java1Practica {
     }
 
     public static void ejercicio4(){
-
+        System.out.print("Enter the value of n: ");
+        int n=input.nextInt();
+        for (int i=0; i<=n; i++){
+            if(isPrime(i)) System.out.print(i+" ");
+        }
     }
 
+    /**
+     * This method checks whether a number is prime or not by
+     * looking for the divisibility of all numbers between 2
+     * and sqrt (n). The justification for sqrt (n) is here:
+     * https://www.wikiprimes.com/como-saber-si-un-numero-es-primo/
+     * @param n -> The number to be analyzed
+     * @return True if its prime, false if isn´t
+     */
     public static boolean isPrime(int n){
         if(n<=1) return false;
         for (int i=2; i<= Math.sqrt(n); i++){
