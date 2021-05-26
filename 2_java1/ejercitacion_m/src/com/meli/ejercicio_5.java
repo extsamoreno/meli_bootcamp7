5f34f6a4750295c7951c2fd35470de2b704f4d19
Florencia Gomez Moreno <florencia.gmoreno@mercadolibre.com>
2021-05-26 15:43:39 -0300

    Fixing exercise 5

--- 2_java1/ejercitacion_m/src/com/meli/ejercicio_5.java
+++ 2_java1/ejercitacion_m/src/com/meli/ejercicio_5.java
@@ -3,18 +3,7 @@ package com.meli;
 import java.util.Scanner;
 
 public class ejercicio_5 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Ingrese la cantidad de numeros que desea: ");
-        int n = scanner.nextInt();
-
-        System.out.println("Ingrese la cantidad de digitos: ");
-        int m = scanner.nextInt();
-
-        System.out.println("Ingrese el digito a controlar: ");
-        int d = scanner.nextInt();
-
+    public static void repeticiones(int n, int m, int d) {
         int i = 0, numeros = 0;
 
         while(i != n) {
@@ -32,8 +21,21 @@ public class ejercicio_5 {
             }
             numeros++;
         }
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ingrese la cantidad de numeros que desea: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Ingrese la cantidad de digitos: ");
+        int m = scanner.nextInt();
+
+        System.out.println("Ingrese el digito a controlar: ");
+        int d = scanner.nextInt();
+
+        repeticiones(n,m,d);
 
         scanner.close();
     }
