8fe3830f1aced93a116fa0addd541dd92f6555fc
Agustin Nicolas Martinez <agustmartine@ar0c02ff0ktml85.ml.com>
2021-05-26 13:23:36 -0300

    clase 1 java numeros par

---
+++ 2_java1/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isPar(int data){
+        return data%2==0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	    System.out.println("Ingrese un número");
+	    int value = 0;
+        value = sc.nextInt();
+        int iterable = 2;
+        System.out.println(0);
+        for(int i=0; i<value-1; i++){
+            System.out.println(iterable);
+            iterable = iterable * 2;
+        }
+    }
+}
