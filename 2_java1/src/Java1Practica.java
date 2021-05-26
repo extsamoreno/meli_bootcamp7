4221e6c2fcf1e12c89803a9b042a25284132b519
Michael Andrey Rivera Castro <mirivera@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-05-26 10:21:05 -0500

    Adding initial Ejercicio4 and correcting isolated case (when n=9) in Ejercicio3

--- 2_java1/src/Java1Practica.java
+++ 2_java1/src/Java1Practica.java
@@ -33,9 +33,13 @@ public class Java1Practica {
         System.out.println(n+" is prime? "+isPrime(n));
     }
 
+    public static void ejercicio4(){
+
+    }
+
     public static boolean isPrime(int n){
         if(n<=1) return false;
-        for (int i=2; i< Math.sqrt(n); i++){
+        for (int i=2; i<= Math.sqrt(n); i++){
             if (n%i==0) return false;
         }
         return true;
