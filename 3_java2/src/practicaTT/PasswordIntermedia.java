5405a9174af5e527c130b6d6b4ef2464b6b21ac6
Gustavo Sosa <gustavo.sosa@mercadolibre.com>
2021-05-27 16:55:23 -0300

    feat:added afternoon exercise

---
+++ 3_java2/src/practicaTT/PasswordIntermedia.java
@@ -0,0 +1,8 @@
+package practicaTT;
+
+public class PasswordIntermedia extends Password {
+    //Minimum eight characters, at least one letter, one number and one special character
+    public PasswordIntermedia() {
+        super("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
+    }
+}
---
+++ 3_java2/src/practicaTT/PasswordSimple.java
@@ -0,0 +1,8 @@
+package practicaTT;
+
+public class PasswordSimple extends Password {
+    //Minimum eight characters, at least one letter and one number
+    public PasswordSimple() {
+        super("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+    }
+}
