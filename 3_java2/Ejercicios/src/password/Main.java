04adb3b9c62809261a16aea1ec72e65bda7675b0
FedeAyme <federico.ayme@mercadolibre.com>
2021-05-27 16:52:27 -0300

    passwords2

---
+++ 3_java2/Ejercicios/src/password/Main.java
@@ -0,0 +1,43 @@
+package password;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Password pswd = new Password("aa");
+
+	    try {
+            pswd.setValue("hola");
+            System.out.println("ok");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("error " + ex.getMessage());
+        }
+
+	    PasswordFuerte strong = new PasswordFuerte();
+
+        try {
+            strong.setValue("Ho?5asds");
+            System.out.println("ok");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("error " + ex.getMessage());
+        }
+
+        PasswordIntermedia medium = new PasswordIntermedia();
+
+        try {
+            medium.setValue("a1%asdert");
+            System.out.println("ok");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("error " + ex.getMessage());
+        }
+
+        PasswordSimple weak = new PasswordSimple();
+
+        try {
+            weak.setValue("asdf1234");
+            System.out.println("ok");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("error " + ex.getMessage());
+        }
+
+    }
+}
---
+++ 3_java2/Ejercicios/src/password/Password.java
@@ -0,0 +1,31 @@
+package password;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Password {
+    private Pattern patron;
+    private String value;
+
+    public Password() {
+
+    }
+
+    public Password(String reg) {
+        this.patron = Pattern.compile(reg);
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String pwd) throws IllegalArgumentException {
+        Matcher matcher = patron.matcher(pwd);
+        boolean matchFound = matcher.find();
+        if(matchFound)
+            this.value = pwd;
+        else
+            throw new IllegalArgumentException("Password not valid");
+
+    }
+}
