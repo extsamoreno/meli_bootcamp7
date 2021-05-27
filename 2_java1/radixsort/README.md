a0735903e7b2df462a192218a7dd9da02dd1d242
Geronimo <geronimo.bazan@mercadolibre.com>
2021-05-26 23:44:14 -0300

    RedixSort solved

---
+++ 2_java1/radixsort/README.md
@@ -0,0 +1,4 @@
+#Resolución de RadixSort
+- La clase RadixSort.java cuenta con el ejecutable y la función radixSort() que permite ordenar un arreglo de int's.
+- La clase StringUtil.java cuenta con la implementación de los métodos dispuestos por los profes.
+
--- 2_java1/radixsort/src/com/bootcamp/RadixSort.java
+++ 2_java1/radixsort/src/com/bootcamp/RadixSort.java
@@ -1,44 +1,73 @@
 package com.bootcamp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RadixSort {
 
-    public static final ArrayList<String>[] LISTS = new ArrayList[10];
-
-    public static final ArrayList<String> L0 = new ArrayList<>();
-    public static final ArrayList<String> L1 = new ArrayList<>();
-    public static final ArrayList<String> L2 = new ArrayList<>();
-    public static final ArrayList<String> L3 = new ArrayList<>();
-    public static final ArrayList<String> L4 = new ArrayList<>();
-    public static final ArrayList<String> L5 = new ArrayList<>();
-    public static final ArrayList<String> L6 = new ArrayList<>();
-    public static final ArrayList<String> L7 = new ArrayList<>();
-    public static final ArrayList<String> L8 = new ArrayList<>();
-    public static final ArrayList<String> L9 = new ArrayList<>();
+    public static final Map<Character, ArrayList<String>> LISTS = new HashMap<>(){{
+        put('0', new ArrayList<>());
+        put('1', new ArrayList<>());
+        put('2', new ArrayList<>());
+        put('3', new ArrayList<>());
+        put('4', new ArrayList<>());
+        put('5', new ArrayList<>());
+        put('6', new ArrayList<>());
+        put('7', new ArrayList<>());
+        put('8', new ArrayList<>());
+        put('9', new ArrayList<>());
+    }};
 
 
     public static void radixSort(int[] arr) {
         //Convertir el arreglo de ints a un arreglo de Strings
         String[] sArray = StringUtil.toStringArray(arr);
 
-        //Normalizar el arreglo según la longitud de sus elementos
+        //Normalizo el arreglo según la longitud de sus elementos
         StringUtil.lNormalize(sArray, '0');
 
+        //Realizo una copia del arreglo de strings pero utilizando un ArrayList
+        ArrayList<String> aux = new ArrayList<>(Arrays.asList(sArray));
+        ArrayList<String> auxInOrder = new ArrayList<>();
 
+        //Obtengo la posicioón del último caracter para cada string
         int lastIndex = sArray[0].length() - 1;
 
-        for (int i = 0; i < sArray.length; i++) {
-            LISTS[sArray[i].charAt(lastIndex)].add(sArray[i]);
+        //Realizo una iteración por los caracteres de cada string de derecha a izquierda
+        for (int i = lastIndex; i >= 0; i--) {
+            auxInOrder.clear();
+
+            //De cada string obtengo el caracter en la posición i y lo agrego en la lista que corresponda
+            //según su valor
+            for (String value: aux) {
+                char key = value.charAt(i);
+                LISTS.get(key).add(value);
+            }
+
+            //En el arrayList auxInOrder almaceno los valores de las listas ordenados por el último caracter
+            //recorrido
+            LISTS.forEach((key, value) -> {
+                auxInOrder.addAll(value);
+                value.clear();
+            });
+
+            aux.clear();
+            aux.addAll(auxInOrder);
         }
 
-        System.out.println();
+        //Transformo el ArrayList de Strings a un Array de Int's
+        int[] result = StringUtil.toIntArray(auxInOrder.toArray(new String[sArray.length]));
 
+        //Replico el resultado en el arreglo de entrada
+        System.arraycopy(result, 0, arr, 0, result.length);
 
     }
 
+
     public static void main(String[] args) {
-        int[] a = {3, 673, 106, 45, 2, 23 };
+        int[] a = {3, 3, 33, 0 , 673, 106, 45, 2, 2300 };
         radixSort(a);
 
         for(int i: a) {
