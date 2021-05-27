c9a7cce11775bdb5d6289f551e653171ba5d1cf7
Gustavo Sosa <gustavo.sosa@mercadolibre.com>
2021-05-26 21:07:40 -0300

    fix: fixed RadixSort

--- 2_java1/src/practicaTT/RadixSort.java
+++ 2_java1/src/practicaTT/RadixSort.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 
 public class RadixSort {
 
-    public static void radixSort(int[] arr) {
+    public static int[] radixSort(int[] arr) {
 
         String[] listOfString = StringUtil.toStringArray(arr);
         StringUtil.lNormalize(listOfString, '0');
@@ -15,68 +15,58 @@ public class RadixSort {
 
         while (digitIndex >= 0) {
 
-            sortByDigitIndex(listOfString, digitIndex);
+            listOfString = sortByDigitIndex(listOfString, digitIndex);
 
             digitIndex--;
         }
 
         arr = StringUtil.toIntArray(listOfString);
 
-        System.out.println("\n\nDentro de radixSort");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + (i < arr.length - 1 ? "," : ""));
-        }
+        return arr;
 
     }
 
-    public static void sortByDigitIndex(String[] listOfString, int digitIndex) {
+    public static String[] sortByDigitIndex(String[] listOfString, int digitIndex) {
         int totalList = 10;
         HashMap<Integer, ArrayList<String>> mapOfList = createMapWithLists(totalList);
 
         for (String elem : listOfString) {
             int digit = Integer.parseInt(String.valueOf(elem.charAt(digitIndex)));
-
             mapOfList.get(digit).add(elem);
         }
-        System.out.println("\n\nDigitIndex:\t" + digitIndex);
-        updateListFromMap(listOfString, mapOfList, totalList);
 
-        System.out.println("\nDentro de sortByDigitIndex");
+        listOfString = updateListFromMap(mapOfList, totalList);
 
-        for (int i = 0; i < listOfString.length; i++) {
-            System.out.print(listOfString[i] + (i < listOfString.length - 1 ? "," : ""));
-        }
 
+        return listOfString;
     }
 
     public static HashMap<Integer, ArrayList<String>> createMapWithLists(int n) {
-        HashMap<Integer, ArrayList<String>> mapOfList = new HashMap<Integer, ArrayList<String>>();
+        HashMap<Integer, ArrayList<String>> mapOfList = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            mapOfList.put(i, new ArrayList<String>());
+            mapOfList.put(i, new ArrayList<>());
         }
         return mapOfList;
     }
 
-    private static void updateListFromMap(String[] listOfString, HashMap<Integer, ArrayList<String>> mapOfList, int totalList) {
+    private static String[] updateListFromMap(HashMap<Integer, ArrayList<String>> mapOfList, int totalList) {
         ArrayList<String> temp = new ArrayList<>();
 
-        for (int i = 0; i < totalList; i++) {
-            temp.addAll(mapOfList.get(i));
-        }
-        System.out.println("Dentro de updateList");
-        listOfString = temp.toArray(new String[0]);
-        for (int i = 0; i < listOfString.length; i++) {
-            System.out.print(listOfString[i] + (i < listOfString.length - 1 ? "," : ""));
-        }
+        for (int i = 0; i < totalList; i++) temp.addAll(mapOfList.get(i));
+
+        return temp.toArray(new String[0]);
     }
 
     public static void main(String[] args) {
 
-        int arr[] = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8};
+        int[] arr = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8};
 
-        radixSort(arr);
+        System.out.println("Before RadixSort:");
+        for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + (i < arr.length - 1 ? "," : ""));
 
-        System.out.println("\n\nDentro de main");
+        arr = radixSort(arr);
+
+        System.out.println("\n\nAfter RadixSort:");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + (i < arr.length - 1 ? "," : ""));
         }
