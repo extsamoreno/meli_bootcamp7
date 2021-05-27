54625faa5ac2de89beae2018d755e6368f667059
Matias Stefanutti <matias.stefanutti@mercadolibre.com>
2021-05-27 10:29:42 -0300

    Initial ejercices

---
+++ 3_java2/src/CuentaCorriente.java
@@ -0,0 +1,12 @@
+public class CuentaCorriente {
+
+    private double saldo = 0.0;
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    public void setSaldo(double s){
+        this.saldo = s;
+    }
+}
