7edd527de81230fbcfd48098afb162cb194f709d
Gustavo Sosa <gustavo.sosa@mercadolibre.com>
2021-05-28 13:22:08 -0300

    feat: java3-changed sortutil

--- 4_java3/src/practicaTM/SortUtil.java
+++ 4_java3/src/practicaTM/SortUtil.java
@@ -18,15 +18,3 @@ public class SortUtil {
 
     }
 }
-/*
-int n = arr.length;
-        for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
-                if (arr[j] > arr[j+1])
-                {
-                    // swap arr[j+1] and arr[j]
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
- */
