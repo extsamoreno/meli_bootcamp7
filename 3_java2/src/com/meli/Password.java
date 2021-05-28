223969ad575da55a2d55d6a05c9afbc2fc56f4ed
Diego Pinto <diego.pinto@mercadolibre.com.co>
2021-05-27 20:30:09 -0500

    Ajustes del punto 3 del Ejercicio 1 de la Clase tarde

--- 3_java2/src/com/meli/Password.java
+++ 3_java2/src/com/meli/Password.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public abstract class Password {
+public class Password {
     private String password;
 
     public Password(String password){
