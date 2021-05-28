3c7d29dfb2fd032c982e4fd3de4a9ea352d8d59e
Florencia Gomez Moreno <florencia.gmoreno@mercadolibre.com>
2021-05-28 16:58:28 -0300

    Starting first evening exercise

---
+++ 4_java3/ejercitacion_t/src/com/meli/BubbleSortSorterImple.java
@@ -0,0 +1,20 @@
+package com.meli;
+
+import java.util.Comparator;
+
+public class BubbleSortSorterImple<T> implements Sorter<T> {
+    @Override
+    public void sort(T[] arr, Comparator<T> c) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if(c.compare(arr[j],arr[j+1]) > 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ 4_java3/ejercitacion_t/src/com/meli/Sorter.java
@@ -0,0 +1,7 @@
+package com.meli;
+
+import java.util.Comparator;
+
+public interface Sorter<T> {
+    public void sort(T arr[], Comparator<T> c);
+}
