6b139d046a764879e44639f05095337df713c9db
Florencia Gomez Moreno <florencia.gmoreno@mercadolibre.com>
2021-05-28 17:50:50 -0300

    Adding changes in evening exercise

--- 4_java3/ejercitacion_t/src/com/meli/Main.java
+++ 4_java3/ejercitacion_t/src/com/meli/Main.java
@@ -5,16 +5,26 @@ import java.util.Comparator;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        Sorter s = (Sorter) MiFactory.getInstance("sorter");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Sorter s = (Sorter<Integer>) MiFactory.getInstance("sorter");
 
         Integer[] iArr = {5,3,0,2,6,9,2};
-        System.out.println(iArr);
+        mostrarArr(iArr);
         Comparator<Integer> c1 = (a,b) -> a-b;
         s.sort(iArr,c1);
-        System.out.println(iArr);
+        mostrarArr(iArr);
 
 //        String[] sArr = {"z", "l", "q", "a", "m"};
+//        Comparator<String> c2 = (s1,s2) -> s1.compareTo(s2);
+//        mostrarArr(sArr);
+//
+//        mostrarArr(sArr);
+    }
 
+    public static <T> void mostrarArr(T[] arr) {
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
+        }
     }
 }
--- 4_java3/ejercitacion_t/src/com/meli/MiFactory.java
+++ 4_java3/ejercitacion_t/src/com/meli/MiFactory.java
@@ -6,13 +6,14 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class MiFactory {
-    public static Object getInstance(String objName) throws IOException {
+    public static Object getInstance(String objName) throws IOException, ClassNotFoundException {
         FileReader reader = new FileReader("/Users/fgmoreno/Documents/it-bootcamp/codes/meli_bootcamp7/4_java3/ejercitacion_t/src/com/meli/MiFactory.properties");
         Properties p = new Properties();
         p.load(reader);
+        System.out.println(p.getProperty("sorter"));
 
-        Class o = p.getProperty(objName).getClass();
-//        Class o = Class.forName(p.getProperty("sorter").getClass().getName());
+//        Class o = p.getProperty(objName).getClass();
+        Class<?> o = Class.forName(p.getProperty("sorter"));
         return o;
     }
 }
