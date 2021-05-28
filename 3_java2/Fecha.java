21fc1ae0a91500ce904ffab1c3fae78beb3cfd97
giancmf <gianfranco.caiti@mercadolibre.com>
2021-05-28 09:54:40 -0300

    fecha

--- 3_java2/Fecha.java
+++ 3_java2/Fecha.java
@@ -1,5 +1,37 @@
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Fecha {
+    int dia,mes,año;
+    private GregorianCalendar calendar;
 
+    public Fecha(int dia, int mes, int año) {
+        this.calendar = (GregorianCalendar) GregorianCalendar.getInstance();
+        this.calendar.set(año,dia,mes);
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+    }
+
+    public void mostrarFecha(){
+        System.out.println(this.calendar.get(Calendar.YEAR)+"/"+this.calendar.get(Calendar.MONTH)+"/"+
+                this.calendar.get(Calendar.DATE));
+    }
+
+    public void sumarDia(int dias){
+        System.out.println("Fecha actual");
+        mostrarFecha();
+        this.calendar.add(Calendar.DATE,dias);
+        System.out.println("Dia sumado");
+        mostrarFecha();
+    }
+
+    @Override
+    public String toString() {
+        return "Fecha{" +
+                "dia=" + dia +
+                ", mes=" + mes +
+                ", año=" + año +
+                '}';
+    }
 }
---
+++ 3_java2/FechaMain.java
@@ -0,0 +1,7 @@
+public class FechaMain {
+    public static void main(String[] args) {
+        Fecha fecha = new Fecha(5,2,2021);
+        fecha.mostrarFecha();
+        fecha.sumarDia(3);
+    }
+}
