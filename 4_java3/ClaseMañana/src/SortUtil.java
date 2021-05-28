f3ee4a47508cd7be6d89ecfaee012f7625348a62
Ignacio Gimenez <ignacio.igimenez@mercadolibre.com>
2021-05-28 15:30:25 -0300

    ordenar2

--- 4_java3/ClaseMañana/src/SortUtil.java
+++ 4_java3/ClaseMañana/src/SortUtil.java
@@ -10,4 +10,21 @@ public class SortUtil {
             }
         }
     }
+
+    public static <T> void ordenar2(Precedable<T>[] arr) {
+        boolean flag = true;
+        int k = 1;
+        while(flag && k <= arr.length) {
+            flag = false;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i].precedeA((T) arr[i + 1]) > 0) {
+                    Precedable<T> aux = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = aux;
+                    flag = true;
+                }
+                k++;
+            }
+        }
+    }
 }
