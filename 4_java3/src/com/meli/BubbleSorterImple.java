3da82087446457a0b57ba135f9383ed9c9a499ff
Federico Giordano <federico.giordano@mercadolibre.com>
2021-05-28 17:05:54 -0300

    4_java3 actividades tt coemzadas

---
+++ 4_java3/src/com/meli/BubbleSorterImple.java
@@ -0,0 +1,22 @@
+package com.meli;
+
+import java.util.Comparator;
+
+//Ahora funciona con genericos, pero de alguna manera rompì la logica y solo los desordena màs Q-Q
+
+public class BubbleSorterImple<T> implements Sorter<T>{
+    @Override
+    public void sort(T arr[] ,Comparator<T> c) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                // is left larger than right?
+                if (c.compare(arr[i],arr[j+1])>0) {
+                    // swap j and j + 1
+                    T tmp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+    }
+}
---
+++ 4_java3/src/com/meli/Main.java
@@ -0,0 +1,19 @@
+package com.meli;
+
+import java.util.Comparator;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Integer[] arr = new Integer[]{9,8,6,7,5,4,3,2,1,0};
+        Comparator<Integer> comp = (n1,n2)->n1-n2;
+        //QuickSorterImple<Integer> sorter = new QuickSorterImple<>();
+        //HeapSorterImple<Integer> sorter = new HeapSorterImple<>();
+        BubbleSorterImple<Integer> sorter = new BubbleSorterImple<>();
+        sorter.sort(arr,comp);
+        for(Integer x : arr ){ System.out.print(x+"-");}
+    }
+}
