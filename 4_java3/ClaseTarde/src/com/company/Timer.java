265f581d1ada18e9aefcf8ffb477f049ad9bf7c3
Antonella Mariel Lesta <alesta@ar0c02ff0l4ml85.ml.com>
2021-05-29 19:55:13 -0300

    clase tarde

---
+++ 4_java3/ClaseTarde/src/com/company/Timer.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Timer {
+    private long start, stop, totalTime;
+
+    public void start() {
+        this.start = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        this.stop = System.currentTimeMillis();
+    }
+
+    public long elapsedTime() {
+        return this.stop - this.start;
+    }
+}
