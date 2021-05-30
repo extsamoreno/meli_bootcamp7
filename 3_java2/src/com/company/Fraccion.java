dbd79a674d9254c2b8c09a478601762459a310bb
gloprete <genaro.loprete@mercadolibre.com>
2021-05-30 20:28:39 -0300

    Advance in the second excercise

--- 3_java2/src/com/company/Fraccion.java
+++ 3_java2/src/com/company/Fraccion.java
@@ -23,7 +23,48 @@ public class Fraccion {
         this.denominador = denominador;
     }
 
-    public Fraccion suma(Fraccion fraccionSumaUna, Fraccion fraccionSumaDos) {
+    //Contructor
+    public Fraccion(double numerador, double denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    private boolean isIgualDenominador(Fraccion fraccionUno, Fraccion fraccionDos) {
+        return fraccionUno.getDenominador() == fraccionDos.getDenominador();
+    }
+
+    private Fraccion sumaDistintoDenominador(Fraccion fraccionSumaUna, Fraccion fraccionSumaDos) {
+        double nuevoNumerador = fraccionSumaUna.getNumerador() + fraccionSumaDos.getNumerador();
+
+        return new Fraccion(nuevoNumerador, fraccionSumaUna.getDenominador());
+    }
+
+    private Fraccion sumaIgualDenominador(Fraccion fraccionSumaUna, Fraccion fraccionSumaDos) {
         return null;
     }
+
+    public Fraccion suma(Fraccion fraccionSumaUna, Fraccion fraccionSumaDos) {
+        if (isIgualDenominador(fraccionSumaUna, fraccionSumaDos))
+            return sumaIgualDenominador(fraccionSumaUna, fraccionSumaDos);
+        else
+            return sumaDistintoDenominador(fraccionSumaUna, fraccionSumaDos);
+    }
+
+    public Fraccion suma(Fraccion fraccionSumaUna, int enteroSuma) {
+        return suma(fraccionSumaUna, new Fraccion(enteroSuma, 1));
+    }
+
+    /*
+    while(i <= num1 || i <= num2)
+        {
+            if(num1%i==0 || num2%i==0)
+            {
+            mcm=mcm*i;
+            if(num1%i==0) num1=num1/i;
+            if(num2%i==0) num2=num2/i;
+            }
+            else
+                i=i+1;
+        }
+     */
 }
