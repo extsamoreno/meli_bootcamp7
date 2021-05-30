06d3b9190f8d42e2f11c1265b5a31612580e9c59
gloprete <genaro.loprete@mercadolibre.com>
2021-05-30 19:34:57 -0300

    Finish part 1

--- 2_java1/java1/src/com/company/Ejercicio5.java
+++ 2_java1/java1/src/com/company/Ejercicio5.java
@@ -20,6 +20,32 @@ public class Ejercicio5 {
 
         int numberToSearch = scanner.nextInt();
 
+        int [] array = searchFirstFiveNumbersWithSomeNumberIn(numberLimit, numberQuantityToSearch, numberToSearch);
+
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
+
+    private static int[] searchFirstFiveNumbersWithSomeNumberIn(int numberLimit, int numberQuantityToSearch, int numberToSearch) {
+
+        int finded = 1;
+        int[] arrayNumbers = new int[numberLimit];
+        int i = 0;
+        while(finded <= numberLimit) {
+            String actualNumberString = Integer.toString(i);
+            char numberToSearchChar = Integer.toString(numberToSearch).charAt(0); //Sacamos el numbero como char
+            int quantityCharacter = getCharacterQuantity(actualNumberString, numberToSearchChar);
+
+            if (quantityCharacter == numberQuantityToSearch) {
+                arrayNumbers[finded - 1] = i;
+                finded++;
+            }
+            i++;
+        }
+
+        return arrayNumbers;
     }
 
     public static void logMessage(String message) {
@@ -28,6 +54,16 @@ public class Ejercicio5 {
 
     //Creamos esta función para encontrar las veces que está un caracter en un string, pasamos el número a string para poder buscarlo
     public static int getCharacterQuantity(String stringToTest, char charToSearch) {
-        return 0;
+        int cant = 0;
+        int stringLength = stringToTest.length();
+
+        for (int i = 0; i < stringLength; i++) {
+            char charActual = stringToTest.charAt(i);
+            if (charActual == charToSearch) {
+                cant++;
+            }
+        }
+
+        return cant;
     }
 }
