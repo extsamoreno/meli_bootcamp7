82cd031c5f16abbc6756b89891eb4a7437f29ed7
faguanco <fabian.fguanco@mercadolibre.com>
2021-05-30 10:42:07 -0300

    finished exercise reflection

--- 4_java3/src/com/company/Main.java
+++ 4_java3/src/com/company/Main.java
@@ -11,7 +11,40 @@ import java.util.Properties;
 public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-	/*
+
+        //testExercises();
+
+        Integer[] arrNumbers = { 10, 7, 8, 9, 1, 5 };
+        String[] arrNames = {"Jorge", "Fabian", "Eduardo", "Florencia", "Sofia"};
+
+        FileReader reader = new FileReader("src/com/company/MiFactory.properties");
+        Properties p = new Properties();
+        p.load(reader);
+
+        String nameObject = p.getProperty("sorter");
+
+        Class<?> objectResult =(Class) MiFactory.getInstance(nameObject);
+
+        Sorter<Integer> instanceIntResult = (Sorter<Integer>) objectResult.getConstructor().newInstance();
+        instanceIntResult.sort(arrNumbers, (a, b) -> b-a);
+
+        Sorter<String> instanceStringResult = (Sorter<String>) objectResult.getConstructor().newInstance();
+        instanceStringResult.sort(arrNames, (a, b) -> a.compareTo(b));
+
+        System.out.println(" Sorted array with " + objectResult.getSimpleName() + ": ");
+        printArray(arrNumbers);
+        printArray(arrNames);
+    }
+
+    public static <T> void printArray(T arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void testExercises() {
         Persona person3 = new Persona("Pers3", 333);
         Persona person2 = new Persona("Pers2", 122);
         Persona persona1 = new Persona("Pers1", 123);
@@ -33,24 +66,6 @@ public class Main {
         for (int i = 0; i < aCelulares.length; i++) {
             System.out.println(aCelulares[i].getCelular());
         }
-	 */
-        Integer[] arr = { 10, 7, 8, 9, 1, 5 };
-
-        FileReader reader = new FileReader("src/com/company/MiFactory.properties");
-
-        Properties p = new Properties();
-
-        p.load(reader);
-
-        String nameObject = p.getProperty("sorter");
-        Class<?> result =(Class) MiFactory.getInstance(nameObject);
-
-        Sorter<Integer> algo = (Sorter<Integer>) result.getConstructor().newInstance();
-
-        algo.sort(arr, (a, b) -> b-a);
-
-        System.out.println("Sorted array: ");
-        BubbleSortSorterImple.printArray(arr);
     }
 }
 
