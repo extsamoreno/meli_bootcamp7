bf6466200642f1e2625cd73b83d5dbe365b985ef
Trinidad Mamud <trinidad.mamud@mercadolibre.com>
2021-05-31 16:59:28 -0300

    Arreglo ruido - Clase 4

--- 5_java4/src/main/java/marteGroupSA/FuegoArtificialIndividual.java
+++ 5_java4/src/main/java/marteGroupSA/FuegoArtificialIndividual.java
@@ -1,18 +1,13 @@
 package marteGroupSA;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class FuegoArtificialIndividual implements FuegoArtificial{
 
+    private String[] ruidos = {"BOOM", "PUM", "KABOOM"};
+
     @Override
     public void hacerRuido() {
-        String s = "";
-        for (int i = 0; i < 5; i++){
-            Random r = new Random();
-            char c = (char)(r.nextInt(26) + 'a');
-            s += c;
-        }
-        System.out.println(s);
+        System.out.println(ruidos[(int) (Math.random() * 3)]);
     }
 }
