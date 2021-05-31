eade1297ef2a5e271c6d0a73ecffeb721600636a
Santiago Garcia <santiago.garcia@mercadolibre.com>
2021-05-31 10:06:28 -0300

    Se agrega factory

---
+++ 4_java3/src/com/ejercicios/BubbleSortSorterImple.java
@@ -0,0 +1,20 @@
+package com.ejercicios;
+
+import java.util.Comparator;
+
+public class BubbleSortSorterImple<T> implements Sorter<T>{
+    @Override
+    public void sort(T[] arr, Comparator<T> c) {
+        int i, j;
+        T aux;
+        for (i = 0; i < arr.length - 1; i++) {
+            for (j = 0; j < arr.length - i - 1; j++) {
+                if (c.compare(arr[j], (T)arr[j + 1]) == 1) {
+                    aux = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = aux;
+                }
+            }
+        }
+    }
+}
--- 4_java3/src/com/ejercicios/MainEjercicioTarde.java
+++ 4_java3/src/com/ejercicios/MainEjercicioTarde.java
@@ -6,12 +6,10 @@ public class MainEjercicioTarde {
 
     public static void main(String[] args) {
 
-
-        Sorter quickSort = new QuickSortSorterImple();
+        /*Sorter quickSort = (Sorter)MiFactory.getInstance("sorter");
         Integer[] arr = {5, 6, 19, 8, 8,89};
         Comparator<Integer> c1 = (a, b)->a - b;
         quickSort.sort(arr, c1);
-
         mostrarArreglo(arr);
 
         Sorter heapSort = new HeapSortSorterSimple();
@@ -19,6 +17,15 @@ public class MainEjercicioTarde {
         heapSort.sort(arr2, c1);
 
         mostrarArreglo(arr2);
+
+        Sorter bubbleSort = new BubbleSortSorterImple();
+        Integer[] arr3 = {5, 6, 19, 8, 8,89};
+        heapSort.sort(arr3, c1);
+
+        mostrarArreglo(arr3);
+        */
+
+
     }
 
     private static <T>void mostrarArreglo(T[] arr) {
