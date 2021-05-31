3171da41c93fcaac17581fc22dca3ef6857f92d1
giancmf <gianfranco.caiti@mercadolibre.com>
2021-05-31 09:29:19 -0300

    fecha

--- 3_java2/Fecha.java
+++ 3_java2/Fecha.java
@@ -3,27 +3,57 @@ import java.util.GregorianCalendar;
 
 public class Fecha {
     int dia,mes,año;
-    private GregorianCalendar calendar;
 
-    public Fecha(int dia, int mes, int año) {
-        this.calendar = (GregorianCalendar) GregorianCalendar.getInstance();
-        this.calendar.set(año,dia,mes);
-        this.dia = dia;
-        this.mes = mes;
-        this.año = año;
+    public Fecha(int dia, int mes, int año) throws Exception {
+        if(isDateValid(año,mes,dia)){
+            this.dia = dia;
+            this.mes = mes;
+            this.año = año;
+        }else{
+            throw new Exception("Error fecha");
+        }
     }
 
     public void mostrarFecha(){
-        System.out.println(this.calendar.get(Calendar.YEAR)+"/"+this.calendar.get(Calendar.MONTH)+"/"+
-                this.calendar.get(Calendar.DATE));
+        System.out.println(this.año+"/"+this.mes+"/"+
+                this.dia);
+    }
+
+    public boolean isDateValid(int año, int mes, int dia) {
+        boolean valid = true;
+        Calendar calendar = new GregorianCalendar(año, mes-1, dia);
+        if (año != calendar.get(Calendar.YEAR)) {
+            valid = false;
+        }
+        else if (mes-1 != calendar.get(Calendar.MONTH)) {
+            valid = false;
+        }
+        else if (dia != calendar.get(Calendar.DAY_OF_MONTH)) {
+            valid = false;
+        }
+        return valid;
     }
 
     public void sumarDia(int dias){
-        System.out.println("Fecha actual");
         mostrarFecha();
-        this.calendar.add(Calendar.DATE,dias);
-        System.out.println("Dia sumado");
+        GregorianCalendar date = new GregorianCalendar(this.año , this.mes,this.dia);
+        date.add(Calendar.DATE,dias);
+        setAño(date.get(Calendar.YEAR));
+        setMes(date.get(Calendar.DATE));
+        setMes(date.get(Calendar.MONTH));
         mostrarFecha();
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
     }
 
     @Override
--- 3_java2/FechaMain.java
+++ 3_java2/FechaMain.java
@@ -1,7 +1,12 @@
 public class FechaMain {
-    public static void main(String[] args) {
-        Fecha fecha = new Fecha(5,2,2021);
-        fecha.mostrarFecha();
-        fecha.sumarDia(3);
+    public static void main(String[] args) throws Exception{
+        try {
+            Fecha fecha = new Fecha(5,5,2021);
+            fecha.sumarDia(2);
+            System.out.println(fecha);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 }
