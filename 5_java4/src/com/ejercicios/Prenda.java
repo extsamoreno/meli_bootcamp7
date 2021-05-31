c6cb430de4fc0d1b699073b1913abf6ba488f085
Santiago Garcia <santiago.garcia@mercadolibre.com>
2021-05-31 11:18:17 -0300

    Se agrega ejercicio de prendas

---
+++ 5_java4/src/com/ejercicios/Prenda.java
@@ -0,0 +1,30 @@
+package com.ejercicios;
+
+public class Prenda {
+    private String marca;
+    private String modelo;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Prenda{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                '}';
+    }
+}
