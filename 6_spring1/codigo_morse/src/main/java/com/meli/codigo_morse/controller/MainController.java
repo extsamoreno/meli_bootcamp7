15813f5f66bde30c584f7f60822f9ab4e3498689
Florencia Gomez Moreno <florencia.gmoreno@mercadolibre.com>
2021-06-01 15:37:02 -0300

    Adding evening exercise

---
+++ 6_spring1/codigo_morse/src/main/java/com/meli/codigo_morse/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.meli.codigo_morse.controller;
+
+import com.meli.codigo_morse.service.MorseDecoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MainController {
+
+    @Autowired
+    MorseDecoder decoder;
+
+    @PostMapping("/decode-morse")
+    public ResponseEntity<?> decodeMorse(@RequestBody Map<String,String> code) {
+        Map<String,Object> response = new HashMap<>();
+        response.put("morse", code.get("code"));
+        response.put("decoding", decoder.decode(code.get("code")));
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
