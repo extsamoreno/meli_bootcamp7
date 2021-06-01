8096c889db767ec3de1333885ac75caab39ae70a
Agustin <agustin.decarlo@mercadolibre.com>
2021-06-01 19:15:13 -0300

    6_spring1 ejercitacion numRomanos y codigoMorse

--- 6_spring1/demo/src/main/java/com/example/demo/GreetingController.java
+++ 6_spring1/demo/src/main/java/com/example/demo/GreetingController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Set;
+
 // Puntos de acceso a la api van a ser a travez de los controladores, se hace atravez de anotations (con arrobas)
 @RestController
 // Con esto defino el mapping
@@ -123,5 +127,81 @@ public class GreetingController {
         }
 
         return ("El resultado es: " + resultado);
+    }
+
+    @GetMapping("morse/{palabra}") //Como se mapea esa funcion get, post. Con esto se crea un endpoint
+    public String getMorse(@PathVariable String palabra){
+        // Tengo que cortar la palabra con un espacio
+
+        String dPalabra = "";
+        String[] mLetras =  palabra.split(" ");
+        Hashtable<Character, String> diccionarioEquivalencias = obtenerEquivalencias();
+
+        // Es necesario realizar esto ya que tenemos que recibir morse, con lo cual no estamos buscando por key, tenemos que iterarlo nosotros
+        // Necesito el value
+        Set<Character> keys = diccionarioEquivalencias.keySet();
+        for(String mLetra: mLetras){
+
+            for(Character key: keys){
+                if(diccionarioEquivalencias.get(key).equals(mLetra)){
+                    dPalabra += key.toString();
+                }
+            }
+        }
+        return dPalabra;
+    }
+
+
+    public static Hashtable<Character, String> obtenerEquivalencias() {
+        Hashtable<Character, String> equivalencias = new Hashtable<>();
+        equivalencias.put('A', ".-");
+        equivalencias.put('B', "-...");
+        equivalencias.put('C', "-.-.");
+        equivalencias.put('D', "-..");
+        equivalencias.put('E', ".");
+        equivalencias.put('F', "..-.");
+        equivalencias.put('G', "--.");
+        equivalencias.put('H', "....");
+        equivalencias.put('I', "..");
+        equivalencias.put('J', ".---");
+        equivalencias.put('K', "-.-");
+        equivalencias.put('L', ".-..");
+        equivalencias.put('M', "--");
+        equivalencias.put('N', "-.");
+        equivalencias.put('Ñ', "--.--");
+        equivalencias.put('O', "---");
+        equivalencias.put('P', ".--.");
+        equivalencias.put('Q', "--.-");
+        equivalencias.put('R', ".-.");
+        equivalencias.put('S', "...");
+        equivalencias.put('T', "-");
+        equivalencias.put('U', "..-");
+        equivalencias.put('V', "...-");
+        equivalencias.put('W', ".--");
+        equivalencias.put('X', "-..-");
+        equivalencias.put('Y', "-.--");
+        equivalencias.put('Z', "--..");
+        equivalencias.put('0', "-----");
+        equivalencias.put('1', ".----");
+        equivalencias.put('2', "..---");
+        equivalencias.put('3', "...--");
+        equivalencias.put('4', "....-");
+        equivalencias.put('5', ".....");
+        equivalencias.put('6', "-....");
+        equivalencias.put('7', "--...");
+        equivalencias.put('8', "---..");
+        equivalencias.put('9', "----.");
+        equivalencias.put('.', ".-.-.-");
+        equivalencias.put(':', "---...");
+        equivalencias.put('?', "..--..");
+        equivalencias.put('-', "-....-");
+        equivalencias.put('/', "-..-.");
+        equivalencias.put('\'', ".-..-.");
+        equivalencias.put('@', ".--.-.");
+        equivalencias.put('=', "-...-");
+        equivalencias.put('!', "−.−.−−");
+        equivalencias.put(' ', " ");
+
+        return equivalencias;
     }
 }
