1043750f1b44fc78a2314e2df703b4fe39cc3a7d
Matias Eduardo Bossa Valverde <mbossa@ar0fvfcg18tl415.ml.com>
2021-06-01 15:58:01 -0300

    Spring_Day_1 Tarde

---
+++ 6_spring1/Spring_Day_1/src/main/java/com/meli/Spring_Day_1/Convert.java
@@ -0,0 +1,126 @@
+package com.meli.Spring_Day_1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.TreeMap;
+
+@RestController
+@RequestMapping("/convert")
+public class Convert {
+
+    private final static TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+
+    static {
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+    }
+
+    @GetMapping("/toRomans/{number}")
+    public String convert(@PathVariable Integer number){
+        int l =  map.floorKey(number);
+        if ( number == l ) {
+            return map.get(number);
+        }
+        return map.get(l) + convert(number-l);
+    }
+
+    @GetMapping("/uncoded/morse/{codificado}")
+    public static String decodificarMorse(@PathVariable String codificado) {
+        StringBuilder decodificado = new StringBuilder();
+        // Necesitamos separarlo por espacios
+        String[] morse = codificado.split(" ");
+
+        for(String item : morse){
+            System.out.println(item);
+        }
+        for (String morseActual : morse) {
+            String equivalencia = morseAAscii(morseActual);
+            decodificado.append(equivalencia);
+        }
+        return decodificado.toString();
+    }
+
+    public static String morseAAscii(String morseBuscado) {
+        Hashtable<String, String> equivalencias = obtenerEquivalencias();
+        Set<String> claves = equivalencias.keySet();
+        // La clave es la letra ASCII
+        for (String clave : claves) {
+            String morse = equivalencias.get(clave);
+            if (morse.equals(morseBuscado)) {
+                return clave;
+            }
+        }
+        return "";
+    }
+
+    public static Hashtable<String, String> obtenerEquivalencias() {
+        Hashtable<String, String> equivalencias = new Hashtable<>();
+        equivalencias.put("A", ".-");
+        equivalencias.put("B", "-...");
+        equivalencias.put("C", "-.-.");
+        equivalencias.put("CH", "----");
+        equivalencias.put("D", "-..");
+        equivalencias.put("E", ".");
+        equivalencias.put("F", "..-.");
+        equivalencias.put("G", "--.");
+        equivalencias.put("H", "....");
+        equivalencias.put("I", "..");
+        equivalencias.put("J", ".---");
+        equivalencias.put("K", "-.-");
+        equivalencias.put("L", ".-..");
+        equivalencias.put("M", "--");
+        equivalencias.put("N", "-.");
+        equivalencias.put("Ñ", "--.--");
+        equivalencias.put("O", "---");
+        equivalencias.put("P", ".--.");
+        equivalencias.put("Q", "--.-");
+        equivalencias.put("R", ".-.");
+        equivalencias.put("S", "...");
+        equivalencias.put("T", "-");
+        equivalencias.put("U", "..-");
+        equivalencias.put("V", "...-");
+        equivalencias.put("W", ".--");
+        equivalencias.put("X", "-..-");
+        equivalencias.put("Y", "-.--");
+        equivalencias.put("Z", "--..");
+        equivalencias.put("0", "-----");
+        equivalencias.put("1", ".----");
+        equivalencias.put("2", "..---");
+        equivalencias.put("3", "...--");
+        equivalencias.put("4", "....-");
+        equivalencias.put("5", ".....");
+        equivalencias.put("6", "-....");
+        equivalencias.put("7", "--...");
+        equivalencias.put("8", "---..");
+        equivalencias.put("9", "----.");
+        equivalencias.put(".", ".-.-.-");
+        equivalencias.put(",", "--..--");
+        equivalencias.put(":", "---...");
+        equivalencias.put("?", "..--..");
+        equivalencias.put("'", ".----.");
+        equivalencias.put("-", "-....-");
+        equivalencias.put("/", "-..-.");
+        equivalencias.put("\"", ".-..-.");
+        equivalencias.put("@", ".--.-.");
+        equivalencias.put("=", "-...-");
+        equivalencias.put("!", "−.−.−−");
+        equivalencias.put(" ", ".......");
+        return equivalencias;
+    }
+}
