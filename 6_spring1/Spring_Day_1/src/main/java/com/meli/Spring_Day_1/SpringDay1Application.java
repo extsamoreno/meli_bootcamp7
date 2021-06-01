79efb282f1f13d046f156ba300368065a8d80e77
Matias Eduardo Bossa Valverde <mbossa@ar0fvfcg18tl415.ml.com>
2021-06-01 14:10:31 -0300

    Spring_Day_1 Mañana

---
+++ 6_spring1/Spring_Day_1/src/main/java/com/meli/Spring_Day_1/SpringDay1Application.java
@@ -0,0 +1,13 @@
+package com.meli.Spring_Day_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDay1Application.class, args);
+	}
+
+}
---
+++ 6_spring1/Spring_Day_1/src/test/java/com/meli/Spring_Day_1/SpringDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.Spring_Day_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
