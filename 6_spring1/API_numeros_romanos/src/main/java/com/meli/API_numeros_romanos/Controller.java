b8daf4eff261d2ddca40f0c28dedc2d2555c3952
Diego Pinto <diego.pinto@mercadolibre.com.co>
2021-06-01 11:55:01 -0500

    Creación del método getMain() como pantalla de bienvenida de la API indicando cómo usar la API

--- 6_spring1/API_numeros_romanos/src/main/java/com/meli/API_numeros_romanos/Controller.java
+++ 6_spring1/API_numeros_romanos/src/main/java/com/meli/API_numeros_romanos/Controller.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/get_roman")
 public class Controller {
 
+    @GetMapping("/")
+    public String getMain() {
+        return "Bienvenido a la API  de numeros romanos, en la rura `localhost:8080/meli/api/get_roman/{number}`" +
+                " podrás recibir el respectivo número romano que coloques en ´{number}´";
+    }
+
     @GetMapping("/{number}")
     public String getRoman (@PathVariable int number) {
         return Roman.convertirANumerosRomanos(number);
