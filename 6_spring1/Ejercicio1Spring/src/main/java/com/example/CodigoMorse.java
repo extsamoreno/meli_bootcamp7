0ad2a8d709b56c9238a62737fc3e83f62f4ba218
Perez Stefania <stefania.perez@merccadolibre.com.co>
2021-06-01 15:28:01 -0500

    Ejercicio 1 Spring

---
+++ 6_spring1/Ejercicio1Spring/src/main/java/com/example/CodigoMorse.java
@@ -0,0 +1,93 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Set;
+
+@RestController
+@RequestMapping
+public class CodigoMorse {
+    @GetMapping("/convertir-codigo-morse/{codigo}")
+    public String convertirCodigoMorse(@PathVariable String codigo) {
+        return decodificarMorse(codigo);
+    }
+    public static HashMap<String, String> obtenerEquivalencias() {
+        HashMap<String, String> equivalencias = new HashMap<>();
+        equivalencias.put("A", ".-");
+        equivalencias.put("B", "-...");
+        equivalencias.put("C", "-.-.");
+        equivalencias.put("CH", "----");
+        equivalencias.put("D", "-..");
+        equivalencias.put("E", ".");
+        equivalencias.put("F", "..-.");
+        equivalencias.put("G", "--.");
+        equivalencias.put("H", "....");
+        equivalencias.put("I", "..");
+        equivalencias.put("J", ".---");
+        equivalencias.put("K", "-.-");
+        equivalencias.put("L", ".-..");
+        equivalencias.put("M", "--");
+        equivalencias.put("N", "-.");
+        equivalencias.put("Ñ", "--.--");
+        equivalencias.put("O", "---");
+        equivalencias.put("P", ".--.");
+        equivalencias.put("Q", "--.-");
+        equivalencias.put("R", ".-.");
+        equivalencias.put("S", "...");
+        equivalencias.put("T", "-");
+        equivalencias.put("U", "..-");
+        equivalencias.put("V", "...-");
+        equivalencias.put("W", ".--");
+        equivalencias.put("X", "-..-");
+        equivalencias.put("Y", "-.--");
+        equivalencias.put("Z", "--..");
+        equivalencias.put("0", "-----");
+        equivalencias.put("1", ".----");
+        equivalencias.put("2", "..---");
+        equivalencias.put("3", "...--");
+        equivalencias.put("4", "....-");
+        equivalencias.put("5", ".....");
+        equivalencias.put("6", "-....");
+        equivalencias.put("7", "--...");
+        equivalencias.put("8", "---..");
+        equivalencias.put("9", "----.");
+        equivalencias.put(".", ".-.-.-");
+        equivalencias.put(",", "--..--");
+        equivalencias.put(":", "---...");
+        equivalencias.put("?", "..--..");
+        equivalencias.put("'", ".----.");
+        equivalencias.put("-", "-....-");
+        equivalencias.put("/", "-..-.");
+        equivalencias.put("\"", ".-..-.");
+        equivalencias.put("@", ".--.-.");
+        equivalencias.put("=", "-...-");
+        equivalencias.put("!", "−.−.−−");
+        return equivalencias;
+    }
+    public static String morseAAscii(String morseBuscado) {
+        HashMap<String, String> equivalencias = obtenerEquivalencias();
+        Set<String> claves = equivalencias.keySet();
+        // La clave es la letra ASCII
+        for (String clave : claves) {
+            String morse = equivalencias.get(clave);
+            if (morse.equals(morseBuscado)) {
+                return clave;
+            }
+        }
+        return "";
+    }
+    public static String decodificarMorse(String codificado) {
+        StringBuilder decodificado = new StringBuilder();
+        // Necesitamos separarlo por espacios
+        String[] morse = codificado.split(" ");
+        for (String morseActual : morse) {
+            String equivalencia = morseAAscii(morseActual);
+            decodificado.append(equivalencia);
+        }
+        return decodificado.toString();
+    }
+}
