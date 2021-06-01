cd9e3715e3391562562bb3cb33f576df6bfdec83
William Prieto <william.prieto@mercadolibre.com.co>
2021-06-01 12:08:35 -0500

    Spring1 - Ejercicio1 - Romanos

---
+++ 6_spring1/Ejercicio1/demo1/src/main/java/com/practica1/demo1/RomanoController.java
@@ -0,0 +1,86 @@
+package com.practica1.demo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/romanos")
+public class RomanoController {
+
+    @GetMapping("/numero/{numero}")
+    public String getRomano(@PathVariable Integer numero){
+        if(numero<1 || numero>3999){
+            return "No se puede convertir";
+        }
+        String romano = convertirANumerosRomanos(numero);
+        return (numero+" = "+romano);
+    }
+
+    //método para pasar a números romanos
+    public static String convertirANumerosRomanos(Integer numero) {
+        int i, miles, centenas, decenas, unidades;
+        String romano = "";
+
+        //obtenemos cada cifra del número
+        miles = numero / 1000;
+        centenas = numero / 100 % 10;
+        decenas = numero / 10 % 10;
+        unidades = numero % 10;
+
+        //millar
+        for (i = 1; i <= miles; i++) {
+            romano = romano + "M";
+        }
+
+        //centenas
+        if (centenas == 9) {
+            romano = romano + "CM";
+        } else if (centenas >= 5) {
+            romano = romano + "D";
+            for (i = 6; i <= centenas; i++) {
+                romano = romano + "C";
+            }
+        } else if (centenas == 4) {
+            romano = romano + "CD";
+        } else {
+            for (i = 1; i <= centenas; i++) {
+                romano = romano + "C";
+            }
+        }
+
+        //decenas
+        if (decenas == 9) {
+            romano = romano + "XC";
+        } else if (decenas >= 5) {
+            romano = romano + "L";
+            for (i = 6; i <= decenas; i++) {
+                romano = romano + "X";
+            }
+        } else if (decenas == 4) {
+            romano = romano + "XL";
+        } else {
+            for (i = 1; i <= decenas; i++) {
+                romano = romano + "X";
+            }
+        }
+
+        //unidades
+        if (unidades == 9) {
+            romano = romano + "IX";
+        } else if (unidades >= 5) {
+            romano = romano + "V";
+            for (i = 6; i <= unidades; i++) {
+                romano = romano + "I";
+            }
+        } else if (unidades == 4) {
+            romano = romano + "IV";
+        } else {
+            for (i = 1; i <= unidades; i++) {
+                romano = romano + "I";
+            }
+        }
+        return romano;
+    }
+}
