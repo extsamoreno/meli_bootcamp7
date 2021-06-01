6ef6952706649cf6508c2645603e2b928c71d646
Geronimo <geronimo.bazan@mercadolibre.com>
2021-06-01 13:34:51 -0300

    first exercise finished

---
+++ 6_spring1/firstproyect/src/main/java/com/bootcamp/firstproyect/FirstproyectApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.firstproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstproyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstproyectApplication.class, args);
+	}
+
+}
