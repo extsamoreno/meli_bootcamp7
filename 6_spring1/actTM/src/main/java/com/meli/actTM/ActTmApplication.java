64792b2a805ac982a5522138e26ee2e09a08024a
Federico Giordano <federico.giordano@mercadolibre.com>
2021-06-01 13:27:49 -0300

    6_spring1 actividad TM terminada

---
+++ 6_spring1/actTM/src/main/java/com/meli/actTM/ActTmApplication.java
@@ -0,0 +1,13 @@
+package com.meli.actTM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActTmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActTmApplication.class, args);
+	}
+
+}
---
+++ 6_spring1/actTM/src/test/java/com/meli/actTM/ActTmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.actTM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActTmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
