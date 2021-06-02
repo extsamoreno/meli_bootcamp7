d3aa1bfc21e5977e6d184f8a8e56f41c8fe0cdda
Mariano Razetto <mariano.razetto@mercadolibre.com>
2021-06-02 15:01:42 -0300

    "Ejercicios TM"

---
+++ 7_spring2/ejercicios_TM/calculadora/src/main/java/com/ejercicios/calculadora/CasaDTO.java
@@ -0,0 +1,13 @@
+package com.ejercicios.calculadora;
+
+public class CasaDTO {
+    private double metrosCuadrados;
+
+    public double getMetrosCuadrados() {
+        return metrosCuadrados;
+    }
+
+    public void setMetrosCuadrados(double metrosCuadrados) {
+        this.metrosCuadrados = metrosCuadrados;
+    }
+}
