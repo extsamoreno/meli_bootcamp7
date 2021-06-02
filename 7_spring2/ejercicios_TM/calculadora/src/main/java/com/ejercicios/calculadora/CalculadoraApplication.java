0ddd407bb32eca5633ffeed200d5e73963667ebe
Mariano Razetto <mariano.razetto@mercadolibre.com>
2021-06-02 15:05:23 -0300

    "Ejercicios TM"

---
+++ 7_spring2/ejercicios_TM/calculadora/src/main/java/com/ejercicios/calculadora/CalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicios.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraApplication.class, args);
+    }
+
+}
---
+++ 7_spring2/ejercicios_TM/calculadora/src/test/java/com/ejercicios/calculadora/CalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicios.calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
