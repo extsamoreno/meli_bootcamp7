362ff704427df196927c5bbadaf359a2f34507e7
Antonella Mariel Lesta <alesta@ar0c02ff0l4ml85.ml.com>
2021-06-02 15:50:20 -0300

    Clase tarde

---
+++ 7_spring2/CalculadoraMetrosCuadrados/src/main/java/com/example/CalculadoraMetrosCuadrados/DiplomaDTO.java
@@ -0,0 +1,22 @@
+package com.example.CalculadoraMetrosCuadrados;
+
+public abstract class DiplomaDTO {
+    private double promedio;
+    private String nombreAlumno;
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    public String getNombreAlumno() {
+        return nombreAlumno;
+    }
+
+    public void setNombreAlumno(String nombreAlumno) {
+        this.nombreAlumno = nombreAlumno;
+    }
+}
---
+++ 7_spring2/CalculadoraMetrosCuadrados/src/main/java/com/example/CalculadoraMetrosCuadrados/DiplomaStandarDTO.java
@@ -0,0 +1,4 @@
+package com.example.CalculadoraMetrosCuadrados;
+
+public class DiplomaStandarDTO extends DiplomaDTO {
+}
