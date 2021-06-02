39d30b8b95009dc8855294d640eae6991940d074
Macarena Agostina Pomerantz <mpomerantz@ar0c02ff0guml85.fibertel.com.ar>
2021-06-02 13:46:27 -0300

    Practica Clase 6 TM

---
+++ 7_spring2/src/main/java/com/example/calculadora/CasaDTO.java
@@ -0,0 +1,13 @@
+package com.example.calculadora;
+
+public class CasaDTO {
+    private double metrosCuadrados;
+
+    public double getMetrosCuadrados() {
+        return metrosCuadrados;
+    }
+
+    public void setMetrosCuadrados(double metrosCuadrados) {
+        this.metrosCuadrados = metrosCuadrados;
+    }
+}
