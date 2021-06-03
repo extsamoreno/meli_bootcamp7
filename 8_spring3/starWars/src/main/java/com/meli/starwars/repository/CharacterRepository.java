37f8e9fddddfc2490cd635fafb4c3e5b0b9e03a3
Gustavo Sosa <gustavo.sosa@mercadolibre.com>
2021-06-03 16:02:06 -0300

    feat: spring3-added star wars exercise

---
+++ 8_spring3/starWars/src/main/java/com/meli/starwars/repository/CharacterRepository.java
@@ -0,0 +1,57 @@
+package com.meli.starwars.repository;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meli.starwars.domain.Character;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CharacterRepository implements ICharacterRepository {
+
+    @Override
+    public List<Character> findCharactersByNameContaining(String name) {
+
+        List<Character> characters = null;
+        characters = loadDatabase();
+
+        List<Character> filteredChar = null;
+        if (characters != null) {
+            filteredChar = characters.stream().filter(c -> nameContaining(c,name)).collect(Collectors.toList());
+
+        }
+        return filteredChar;
+    }
+
+    private boolean nameContaining(Character c, String name) {
+        return c.getName().toLowerCase().contains(name.toLowerCase());
+    }
+
+
+    private List<Character> loadDatabase() {
+        File file = null;
+        try {
+            file = ResourceUtils.getFile("classpath:starwars.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<Character>> typeRef = new TypeReference<>() {
+        };
+        List<Character> characters = null;
+        try {
+            characters = objectMapper.readValue(file, typeRef);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return characters;
+    }
+
+
+}
---
+++ 8_spring3/starWars/src/main/java/com/meli/starwars/repository/ICharacterRepository.java
@@ -0,0 +1,9 @@
+package com.meli.starwars.repository;
+
+import com.meli.starwars.domain.Character;
+
+import java.util.List;
+
+public interface ICharacterRepository {
+    public List<Character> findCharactersByNameContaining(String name);
+}
