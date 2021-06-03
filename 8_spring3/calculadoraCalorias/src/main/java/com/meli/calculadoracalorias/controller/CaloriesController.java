2a87d905db344578ae7a2f948d8d2b83116569d9
Gustavo Sosa <gustavo.sosa@mercadolibre.com>
2021-06-03 14:12:48 -0300

    feat: spring3-working calorias exercise

---
+++ 8_spring3/calculadoraCalorias/src/main/java/com/meli/calculadoracalorias/controller/CaloriesController.java
@@ -0,0 +1,28 @@
+package com.meli.calculadoracalorias.controller;
+
+import com.meli.calculadoracalorias.domain.Dish;
+import com.meli.calculadoracalorias.service.ICaloriesService;
+import com.meli.calculadoracalorias.service.dto.DishDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculate-calories")
+public class CaloriesController {
+
+    @Autowired
+    ICaloriesService caloriesService;
+
+    @PostMapping("/dish")
+    public ResponseEntity<DishDTO> dishData(@RequestBody Dish dish){
+        return new ResponseEntity<>(caloriesService.getDishData(dish), HttpStatus.OK);
+    }
+
+
+}
---
+++ 8_spring3/calculadoraCalorias/src/main/java/com/meli/calculadoracalorias/service/CaloriesService.java
@@ -0,0 +1,56 @@
+package com.meli.calculadoracalorias.service;
+
+import com.meli.calculadoracalorias.domain.Dish;
+import com.meli.calculadoracalorias.domain.Ingredient;
+import com.meli.calculadoracalorias.repository.IFoodRepository;
+import com.meli.calculadoracalorias.service.dto.DishDTO;
+import com.meli.calculadoracalorias.service.dto.IngredientDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CaloriesService implements ICaloriesService {
+
+    @Autowired
+    IFoodRepository foodRepository;
+
+    @Override
+    public DishDTO getDishData(Dish dish) {
+        List<IngredientDTO> ingredients = getIngredientsCaloriesByWeight(dish.getIngredients());
+        double totalCalories = getTotalCalories(ingredients);
+        IngredientDTO maxCalories = getMaxCaloriesIngredient(ingredients);
+        return new DishDTO(dish.getName(), totalCalories, ingredients, maxCalories);
+    }
+
+
+    private List<IngredientDTO> getIngredientsCaloriesByWeight(List<Ingredient> ingredients) {
+        return ingredients.stream().map(
+                i -> new IngredientDTO(i.getName(), getIngredientTotalCalories(i))
+        ).collect(Collectors.toList());
+    }
+
+    private double getIngredientTotalCalories(Ingredient i) {
+        return i.getWeight() * getCaloriesByName(i.getName());
+    }
+
+    private double getCaloriesByName(String name) {
+        return getIngredientByName(name).getCalories();
+    }
+
+    private IngredientDTO getIngredientByName(String name) {
+        return foodRepository.findIngredientByName(name);
+    }
+
+    private double getTotalCalories(List<IngredientDTO> ingredients) {
+        return ingredients.stream().mapToDouble(i -> i.getCalories()).sum();
+    }
+
+    private IngredientDTO getMaxCaloriesIngredient(List<IngredientDTO> ingredients) {
+        return ingredients.stream().max(Comparator.comparing(IngredientDTO::getCalories)).get();
+    }
+
+}
