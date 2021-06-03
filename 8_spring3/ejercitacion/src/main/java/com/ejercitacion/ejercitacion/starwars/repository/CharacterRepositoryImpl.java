86b8c90a161595c248f602981295fe57d78323e0
axdietrich <axel.adietrich@mercadolibre.com>
2021-06-03 17:02:24 -0300

    Ejercicios clase 3 Spring realizados

--- 8_spring3/ejercitacion/src/main/java/com/ejercitacion/ejercitacion/starwars/repository/CharacterRepositoryImpl.java
+++ 8_spring3/ejercitacion/src/main/java/com/ejercitacion/ejercitacion/starwars/repository/CharacterRepositoryImpl.java
@@ -21,7 +21,8 @@ public class CharacterRepositoryImpl implements CharacterRepository {
         List<StarWarsCharacter> characters = loadDatabase();
         List<StarWarsCharacter> result = new ArrayList<>();
         if (characters != null) {
-           result = characters.stream().filter(c -> c.getName().toUpperCase().trim().contains(name.toUpperCase())).collect(Collectors.toList());
+           result = characters.stream().filter(c -> c.getName().toUpperCase().replaceAll("\\s", "")
+                               .contains(name.toUpperCase().replaceAll("\\s", ""))).collect(Collectors.toList());
         }
         return result;
     }
