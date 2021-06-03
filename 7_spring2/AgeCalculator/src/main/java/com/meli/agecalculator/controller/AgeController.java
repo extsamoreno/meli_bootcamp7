d10ad9833a5dfd9e63944c40764f2e5d00b6b7de
jhadermanuel <jhadermanuel.hurtado@mercadolibre.com.co>
2021-06-02 22:36:47 -0500

    update agecontroller

--- 7_spring2/AgeCalculator/src/main/java/com/meli/agecalculator/controller/AgeController.java
+++ 7_spring2/AgeCalculator/src/main/java/com/meli/agecalculator/controller/AgeController.java
@@ -4,6 +4,7 @@ import com.meli.agecalculator.service.IAgeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class AgeController {
     @Autowired
     private IAgeService ageService;
 
+    @GetMapping("/{day}/{moth}/{year}")
     public ResponseEntity<Integer> calculateAge(@PathVariable int day, @PathVariable int month, @PathVariable int year) {
         return new ResponseEntity<Integer>(ageService.calculateAge(day, month, year), HttpStatus.OK);
     }
