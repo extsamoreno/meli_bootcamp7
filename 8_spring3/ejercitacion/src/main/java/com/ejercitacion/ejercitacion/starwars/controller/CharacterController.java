418adbaf1bc50ad43f4d979c20807c23652f39fd
axdietrich <axel.adietrich@mercadolibre.com>
2021-06-03 16:48:39 -0300

    Ejercicios clase 3 Spring realizados

--- 8_spring3/ejercitacion/src/main/java/com/ejercitacion/ejercitacion/starwars/controller/CharacterController.java
+++ 8_spring3/ejercitacion/src/main/java/com/ejercitacion/ejercitacion/starwars/controller/CharacterController.java
@@ -4,10 +4,7 @@ import com.ejercitacion.ejercitacion.starwars.dto.StarWarsCharacterDTO;
 import com.ejercitacion.ejercitacion.starwars.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +15,7 @@ public class CharacterController {
     @Autowired
     private CharacterService service;
 
-    @PostMapping("/{name}")
+    @GetMapping("/{name}")
     public ResponseEntity<List<StarWarsCharacterDTO>> getCharactersByName(@PathVariable String name) {
         return ResponseEntity.ok(service.getCharactersByName(name));
     }
