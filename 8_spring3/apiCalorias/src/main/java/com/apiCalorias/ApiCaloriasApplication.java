8498197b4ccb921c012bbf45597efe7e31ae8c8c
FedeAyme <federico.ayme@mercadolibre.com>
2021-06-03 12:45:32 -0300

    first commit

---
+++ 8_spring3/apiCalorias/src/main/java/com/apiCalorias/ApiCaloriasApplication.java
@@ -0,0 +1,13 @@
+package com.apiCalorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCaloriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCaloriasApplication.class, args);
+	}
+
+}
