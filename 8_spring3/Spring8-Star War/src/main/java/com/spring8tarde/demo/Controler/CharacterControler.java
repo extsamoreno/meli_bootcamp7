4cfd2a65f81d44e4822df6c38b59fd3851369086
nian-meli <nicoll.mejia@mercadolibre.com.co>
2021-06-04 11:20:27 -0500

    Add ResponseEntity

--- 8_spring3/Spring8-Star War/src/main/java/com/spring8tarde/demo/Controler/CharacterControler.java
+++ 8_spring3/Spring8-Star War/src/main/java/com/spring8tarde/demo/Controler/CharacterControler.java
@@ -5,6 +5,8 @@ import com.spring8tarde.demo.Model.DTO.CharacterDTOIN;
 import com.spring8tarde.demo.Model.DTO.CharacterDTOOUT;
 import com.spring8tarde.demo.Service.ICharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +23,7 @@ public class CharacterControler{
     ICharacterService iCharacterService;
 
     @PostMapping("/getCharacter")
-    public ArrayList<CharacterDTOOUT> getCharacter(@RequestBody CharacterDTOIN characterDTOIN) throws IOException {
-        return iCharacterService.getCharacter(characterDTOIN);
+    public ResponseEntity<ArrayList<CharacterDTOOUT>> getCharacter(@RequestBody CharacterDTOIN characterDTOIN) throws IOException {
+        return new ResponseEntity<>(iCharacterService.getCharacter(characterDTOIN), HttpStatus.OK);
     }
 }
