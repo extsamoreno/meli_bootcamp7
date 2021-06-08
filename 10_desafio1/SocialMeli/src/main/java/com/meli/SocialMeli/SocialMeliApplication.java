c265d3481f09becfa687e1c71b2bacbaff96df76
Federico Giordano <federico.giordano@mercadolibre.com>
2021-06-08 11:04:04 -0300

    archviso reorganizados

---
+++ 10_desafio1/SocialMeli/src/main/java/com/meli/SocialMeli/SocialMeliApplication.java
@@ -0,0 +1,13 @@
+package com.meli.SocialMeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMeliApplication.class, args);
+	}
+
+}
