36d2643e583a0572e87f24feee7ae91d3f251f70
Matias Stefanutti <matias.stefanutti@mercadolibre.com>
2021-06-08 08:38:13 -0300

    initial commit

---
+++ 10_desafio1/springchallenge/src/main/java/meli/springchallenge/SpringchallengeApplication.java
@@ -0,0 +1,13 @@
+package meli.springchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringchallengeApplication.class, args);
+	}
+
+}
