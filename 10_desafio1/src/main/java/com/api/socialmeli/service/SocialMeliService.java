8baf84f79296d77d46f7bbb2bc174a71b11a6dea
Mariano Razetto <mariano.razetto@mercadolibre.com>
2021-06-08 18:31:10 -0300

    "first push"

--- 10_desafio1/src/main/java/com/api/socialmeli/service/SocialMeliService.java
+++ 10_desafio1/src/main/java/com/api/socialmeli/service/SocialMeliService.java
@@ -3,9 +3,8 @@ package com.api.socialmeli.service;
 
 import com.api.socialmeli.dto.FollowersCountDTO;
 import com.api.socialmeli.dto.FollowersDetailDTO;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Service
 public interface SocialMeliService {
