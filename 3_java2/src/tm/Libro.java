3ef70d81998e6c4b80277146d08cf12633b02a07
diego-banfi-meli <diego.banfi@mercadolibre.com>
2021-06-08 20:34:35 -0300

    Ejcio Libro 3_java2

---
+++ 3_java2/src/tm/Libro.java
@@ -0,0 +1,64 @@
+package tm;
+
+public class Libro {
+
+    private String titulo;
+    private String isbn;
+    private String autor;
+    private boolean prestado;
+
+    public void prestamo() {
+        this.setPrestado(true);
+    }
+
+    public void devolucion() {
+        this.setPrestado(false);
+    }
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String isbn, String autor, boolean prestado) {
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.autor = autor;
+        this.prestado = prestado;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public boolean isPrestado() {
+        return prestado;
+    }
+
+    public void setPrestado(boolean prestado) {
+        this.prestado = prestado;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + ", " + isbn + ", " + autor + " (prestado = " + prestado + ')';
+    }
+}
