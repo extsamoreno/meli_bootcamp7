e25eff3897e5c58da4e14c49f2397a8f0a96857d
Antonella Mariel Lesta <alesta@ar0c02ff0l4ml85.ml.com>
2021-06-08 12:02:15 -0300

    primmer commit

---
+++ 10_desafio1/DesafioSpring/src/main/java/com/example/DesafioSpring/controllers/SocialMeliController.java
@@ -0,0 +1,22 @@
+package com.example.DesafioSpring.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users/123")
+public class SocialMeliController {
+
+    //autowired
+
+   // @GetMapping("/follow/{userIdToFollow}")
+
+
+}
---
+++ 10_desafio1/DesafioSpring/src/test/java/com/example/DesafioSpring/DesafioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DesafioSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
