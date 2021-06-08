39f945b88fc70caefd54ea98516bd6f6464a34f0
faguanco <fabian.fguanco@mercadolibre.com>
2021-06-08 11:12:34 -0300

    init commit for social media

---
+++ 10_desafio1/socialmedia/src/main/java/com/socialmedia/socialmedia/SocialmediaApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaApplication.class, args);
+	}
+
+}
