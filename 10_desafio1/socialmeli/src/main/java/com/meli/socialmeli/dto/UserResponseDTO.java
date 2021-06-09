76c144033fa7cf393a2444a694f3f9603c8b80c4
andradew-meli <wilson.andrade@mercadolibre.com.co>
2021-06-09 12:38:38 -0500

    Puntos 1, 2, 3 listos (Correccion)

--- 10_desafio1/socialmeli/src/main/java/com/meli/socialmeli/dto/UserResponseDTO.java
+++ 10_desafio1/socialmeli/src/main/java/com/meli/socialmeli/dto/UserResponseDTO.java
@@ -1,18 +1,13 @@
 package com.meli.socialmeli.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
 @AllArgsConstructor
 public class UserResponseDTO {
-<<<<<<< HEAD
 
     private int userId;
     private String userName;
     //private String typeOfFollow;
-=======
-    private int userId;
-    private String UserName;
-
-
->>>>>>> 97ccd39f43a35ef07d0dfa9228f1ac61e497f5b6
 }
