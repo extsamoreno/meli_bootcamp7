e9aea2f11f443a051cdc8fc13260efb92c4d3cf7
jhadermanuel <jhadermanuel.hurtado@mercadolibre.com.co>
2021-06-09 14:39:34 -0500

    Add US009

--- 10_desafio1/socialmeli/src/main/java/com/meli/socialmeli/service/PostServiceImpl.java
+++ 10_desafio1/socialmeli/src/main/java/com/meli/socialmeli/service/PostServiceImpl.java
@@ -13,8 +13,8 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,6 +30,8 @@ public class PostServiceImpl implements IPostService {
     private IUserService userService;
 
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    private final Comparator<PostDTO> comparatorDateDesc = (a, b) -> LocalDate.parse(a.getDate(), formatter).compareTo(LocalDate.parse(b.getDate(), formatter));
+    private final Comparator<PostDTO> comparatorDateAsc = (a, b) -> LocalDate.parse(b.getDate(), formatter).compareTo(LocalDate.parse(a.getDate(), formatter));
 
     @Override
     public void addPost(PostDTO post) {
@@ -55,14 +57,10 @@ public class PostServiceImpl implements IPostService {
         List<PostDTO> postsListFollowingUsers = new ArrayList<>();
         followings.forEach(e -> postsListFollowingUsers.addAll(getListPostByUserId(e)
                 .stream()
-                .filter(x -> LocalDate.parse(x.getDate(),formatter).isAfter(LocalDate.now().minusWeeks(2)))
+                .filter(x -> LocalDate.parse(x.getDate(), formatter).isAfter(LocalDate.now().minusWeeks(2)))
                 .collect(Collectors.toList())
         ));
-        if (order !=null && order.compareTo("desc") > 0) {
-            postsListFollowingUsers.sort((a, b) -> LocalDate.parse(a.getDate(),formatter).compareTo(LocalDate.parse(b.getDate(),formatter)));
-        } else {
-            postsListFollowingUsers.sort((a, b) -> LocalDate.parse(b.getDate(),formatter).compareTo(LocalDate.parse(a.getDate(),formatter)));
-        }
+        sortListByOrder(postsListFollowingUsers, order);
         return new PostFollowingDTO(userId, postsListFollowingUsers);
     }
 
@@ -76,13 +74,20 @@ public class PostServiceImpl implements IPostService {
                 .filter(x -> LocalDate.parse(x.getDate()).isAfter(LocalDate.now().minusWeeks(2)))
                 .collect(Collectors.toList())
         ));
-        if (order !=null && order.compareTo("desc") > 0) {
-            postsListFollowingUsers.sort((a, b) -> LocalDate.parse(a.getDate(),formatter).compareTo(LocalDate.parse(b.getDate(),formatter)));
-        } else {
-            postsListFollowingUsers.sort((a, b) -> LocalDate.parse(b.getDate(),formatter).compareTo(LocalDate.parse(a.getDate(),formatter)));
-        }
+        sortListByOrder(postsListFollowingUsers, order);
 
         return new PostFollowingDTO(userId, postsListFollowingUsers);
     }
 
+    private void sortListByOrder(List<PostDTO> list, String order) {
+        if (order != null && order.compareTo("date_asc") < 0) {
+            sortList(list, comparatorDateAsc);
+        } else {
+            sortList(list, comparatorDateDesc);
+        }
+    }
+
+    private void sortList(List<PostDTO> list, Comparator<PostDTO> comparator) {
+        list.sort(comparator);
+    }
 }
