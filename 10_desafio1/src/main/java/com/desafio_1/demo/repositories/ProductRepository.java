0c5a5f5b79351c68628ba83344a429763e2c201e
Santiago Garcia <santiago.garcia@mercadolibre.com>
2021-06-09 11:34:40 -0300

    Se agrega autogeneracion de id y fecha actual

--- 10_desafio1/src/main/java/com/desafio_1/demo/repositories/ProductRepository.java
+++ 10_desafio1/src/main/java/com/desafio_1/demo/repositories/ProductRepository.java
@@ -3,8 +3,11 @@ package com.desafio_1.demo.repositories;
 import com.desafio_1.demo.dtos.ProductRequestDTO;
 import com.desafio_1.demo.exceptions.UnhandledException;
 import com.desafio_1.demo.models.Product;
+import com.desafio_1.demo.models.ProductDetail;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 @Repository
@@ -16,6 +19,12 @@ public class ProductRepository implements IProductRepository{
     public Product addProduct(Product product) throws UnhandledException {
 
         try{
+            int id = listProducts.size()+1;
+            ProductDetail detail = product.getDetail();
+
+            product.setIdPost(id);
+            product.setDate(LocalDate.now());
+            product.setDetail(new ProductDetail(id, detail.getProductName(),detail.getType(), detail.getBrand(), detail.getColor(),detail.getNotes()));
             listProducts.add(product);
             return product;
         }catch(Exception ex){
