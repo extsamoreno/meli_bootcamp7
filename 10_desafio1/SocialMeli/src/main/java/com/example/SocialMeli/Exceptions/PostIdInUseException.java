57eacdeb4035b194d9bf0610deff8e25813e588e
alaranda <alexis.aranda@mercadolibre.cl>
2021-06-10 14:28:56 -0400

    refactor: move postman collection to root file

--- 10_desafio1/SocialMeli/src/main/java/com/example/SocialMeli/Exceptions/PostIdInUseException.java
+++
@@ -1,9 +0,0 @@
-package com.example.SocialMeli.Exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class ProductIdInUseException extends UserException{
-    public ProductIdInUseException(int productId) {
-        super("Product: "+productId+" is already being used", HttpStatus.BAD_REQUEST);
-    }
-}
--- 10_desafio1/SocialMeli/src/main/java/com/example/SocialMeli/Exceptions/ProductIdInUseException.java
+++
@@ -1,9 +0,0 @@
-package com.example.SocialMeli.Exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class ProductNotFoundException extends UserException{
-    public ProductNotFoundException(int UserId) {
-        super("Product: "+UserId+" not found", HttpStatus.BAD_REQUEST);
-    }
-}
