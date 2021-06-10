130e82107960c87eeb2a19243989bf3a22268c7e
Nicolas Martin Sapaya <nicolas.sapaya@mercadolibre.com>
2021-06-10 15:32:28 -0300

    Optimize imports

--- 10_desafio1/desafio1/src/main/java/com/example/desafio1/controllers/ProductController.java
+++ 10_desafio1/desafio1/src/main/java/com/example/desafio1/controllers/ProductController.java
@@ -2,7 +2,6 @@ package com.example.desafio1.controllers;
 
 import com.example.desafio1.dtos.*;
 import com.example.desafio1.exceptions.product.ProductException;
-import com.example.desafio1.exceptions.product.SameIdPostException;
 import com.example.desafio1.exceptions.user.InvalidUserIdException;
 import com.example.desafio1.exceptions.user.UserException;
 import com.example.desafio1.services.IProductService;
