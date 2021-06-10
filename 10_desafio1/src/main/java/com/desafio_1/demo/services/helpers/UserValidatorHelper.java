d238f5011b323fefe0b743cbeb3dc45067274195
Santiago Garcia <santiago.garcia@mercadolibre.com>
2021-06-10 17:00:22 -0300

    Component deleted

--- 10_desafio1/src/main/java/com/desafio_1/demo/services/helpers/UserValidatorHelper.java
+++ 10_desafio1/src/main/java/com/desafio_1/demo/services/helpers/UserValidatorHelper.java
@@ -3,17 +3,12 @@ package com.desafio_1.demo.services.helpers;
 import br.com.fluentvalidator.Validator;
 import br.com.fluentvalidator.context.ValidationResult;
 import com.desafio_1.demo.exceptions.BadRequestException;
-import com.desafio_1.demo.exceptions.UnhandledException;
 import com.desafio_1.demo.models.User;
-import com.desafio_1.demo.repositories.IUserRepository;
 import com.desafio_1.demo.services.validators.UserExistValidator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
 public class UserValidatorHelper {
 
-    public static void validateUserExist(User user) throws BadRequestException, UnhandledException {
+    public static void validateUserExist(User user) throws BadRequestException{
         Validator<User> validator = new UserExistValidator();
         ValidationResult result = validator.validate(user);
         if(!result.isValid()){
