c2bd309cd729c74d29acf55e8ad44601b8933716
Macarena Agostina Pomerantz <mpomerantz@ar0c02ff0guml85.fibertel.com.ar>
2021-06-10 14:32:12 -0300

    Desafio 1 API Documentation

--- 10_desafio1/src/main/java/com/example/socialmeli/controllers/ProductController.java
+++ 10_desafio1/src/main/java/com/example/socialmeli/controllers/ProductController.java
@@ -11,6 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * SocialMeli REST API - ProductController
+ *
+ * @author Macarena Agostina Pomerantz
+ */
+
 @RestController
 @RequestMapping("/socialmeli/products")
 public class ProductController {
@@ -18,25 +24,70 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+    /**
+     * Adds a new post to a user
+     *
+     * @param newPost the post and userId in which you want to add the post
+     * @return the status and message of success in follow request
+     * @throws InexistentUserException if the user does not exist in the local repository
+     * @throws ExistentPostException if the post is already added to the user
+     */
+
     @PostMapping("/newpost")
     public ResponseEntity<NewPostResponseDTO> addPost (@RequestBody NewPostRequestDTO newPost) throws InexistentUserException, ExistentPostException {
         return new ResponseEntity<>(productService.addPost (newPost), HttpStatus.OK);
     }
+
+    /**
+     * Lists last 2 weeks posts of followed users
+     *
+     * @param userId the userId of the user you want to lists its last 2 weeks followed posts
+     * @param order the order type of the response. It can be "date_asc" or "date_desc". If nothing is sent, default value is set to "date_asc"
+     * @return the userId of the user and its last 2 weeks followed users posts
+     * @throws InexistentUserException if the user does not exist in the local repository
+     * @throws InexistentDateOrderException if the order provided is not valid and it is not "date_asc" or "date_desc"
+     */
 
     @GetMapping("/followed/{userId}/list")
     public ResponseEntity<ListFollowedPostsResponseDTO> listPosts (@PathVariable int userId, @RequestParam(required = false, defaultValue = "date_asc") String order) throws InexistentUserException, InexistentDateOrderException {
         return new ResponseEntity<>(productService.listPosts(userId, order), HttpStatus.OK);
     }
 
+    /**
+     * Adds a new promo post to a user
+     *
+     * @param newPost the promo post and userId in which you want to add the promo post
+     * @return the status and message of success in follow request
+     * @throws InexistentUserException if the user does not exist in the local repository
+     * @throws ExistentPromoPostException if the promo post is already added to the user
+     * @throws InvalidPromoPostException if the promo post is not sent as a promo post
+     */
+
     @PostMapping("/newpromopost")
     public ResponseEntity<NewPostResponseDTO> addPromoPost (@RequestBody NewPromoPostRequestDTO newPost) throws InexistentUserException, ExistentPromoPostException, InvalidPromoPostException {
         return new ResponseEntity<>(productService.addPromoPost (newPost), HttpStatus.OK);
     }
 
+    /**
+     * Counts promo posts
+     *
+     * @param userId the userId of the user to count its promo post
+     * @return the userId and userName of the user and its quantity of promo posts
+     * @throws InexistentUserException if the user does not exist in the local repository
+     */
+
     @GetMapping("/{userId}/countPromo")
     public ResponseEntity<SellerPromoProductsCountResponseDTO> countPromoProducts (@PathVariable int userId) throws InexistentUserException {
         return new ResponseEntity<>(productService.countPromoProducts(userId), HttpStatus.OK);
     }
+
+    /**
+     * Lists user promo post
+     *
+     * @param userId the userId of the user to list its promo post
+     * @return the userId and userName of the user and a list of its promo posts
+     * @throws InexistentUserException if the user does not exist in the local repository
+     */
 
     @GetMapping("/{userId}/list")
     public ResponseEntity<ListSellerPromoProductsDTO> listPromoProducts (@PathVariable int userId) throws InexistentUserException {
--- 10_desafio1/src/main/java/com/example/socialmeli/controllers/UserController.java
+++ 10_desafio1/src/main/java/com/example/socialmeli/controllers/UserController.java
@@ -9,6 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * SocialMeli REST API - UserController
+ *
+ * @author Macarena Agostina Pomerantz
+ */
+
 @RestController
 @RequestMapping("/socialmeli/users")
 public class UserController {
@@ -16,30 +22,88 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    /**
+     * Creates a new user in local repository
+     *
+     * @param newUser the user you want to create in the local repository
+     * @return the user that was created in the local repository userName and userId
+     * @throws ExistentUserException if the user already exists in the local repository
+     * @throws InvalidUserException if the userName provided is empty or not valid
+     */
+
     @PostMapping("/addOne")
     public ResponseEntity<NewUserResponseDTO> addUser (@RequestBody NewUserRequestDTO newUser) throws ExistentUserException, InvalidUserException {
         return new ResponseEntity<>(userService.addUser(newUser), HttpStatus.CREATED);
     }
+
+    /**
+     * Follows a user
+     *
+     * @param userId the userId of the folower user
+     * @param userIdToFollow the userId of the user to follow
+     * @return the status and message of success in follow request
+     * @throws InexistentUserException if the follower user or user to follow does not exist in the local repository
+     * @throws ExistentFollowerException if the follower user already follows the user to follow
+     * @throws InvalidFollowerException if a follower user wants to follow itself
+     */
 
     @PostMapping("/{userId}/follow/{userIdToFollow}")
     public ResponseEntity<FollowSellerResponseDTO> followSeller (@PathVariable int userId, @PathVariable int userIdToFollow) throws InexistentUserException, ExistentFollowerException, InvalidFollowerException {
         return new ResponseEntity<>(userService.followSeller(userId, userIdToFollow), HttpStatus.OK);
     }
 
+    /**
+     * Counts user followers
+     *
+     * @param userId the userId of the user to count its followers
+     * @return the userId and userName of the user and its quantity of followers
+     * @throws InexistentUserException if the user does not exist in th local repository
+     */
+
     @GetMapping("/{userId}/followers/count")
     public ResponseEntity<FollowersCountResponseDTO> countFollowers (@PathVariable int userId) throws InexistentUserException {
         return new ResponseEntity<>(userService.countFollowers(userId), HttpStatus.OK);
     }
+
+    /**
+     * Lists user followers
+     *
+     * @param userId the userId of the user to list its followers
+     * @param order the order type of the response. It can be "name_asc" or "name_desc". If nothing is sent, default value is set to "name_asc"
+     * @return the userId and userName of the user and a list of its followers with their names and ids
+     * @throws InexistentUserException if the user does not exist in th local repository
+     * @throws InexistentNameOrderException if the order provided is not valid and it is not "name_asc" or "name_desc"
+     */
 
     @GetMapping("/{userId}/followers/list")
     public ResponseEntity<ListFollowersResponseDTO> listFollowers (@PathVariable int userId, @RequestParam(required = false, defaultValue = "name_asc") String order) throws InexistentUserException, InexistentNameOrderException {
         return new ResponseEntity<>(userService.listFollowers(userId, order), HttpStatus.OK);
     }
 
+    /**
+     * Lists followed users
+     *
+     * @param userId the userId of the user to list the user it follows
+     * @param order the order type of the response. It can be "name_asc" or "name_desc". If nothing is sent, default value is set to "name_asc"
+     * @return the userId and userName of the user and a list of its followed users with their names and ids
+     * @throws InexistentUserException if the user does not exist in th local repository
+     * @throws InexistentNameOrderException if the order provided is not valid and it is not "name_asc" or "name_desc"
+     */
+
     @GetMapping("/{userId}/followed/list")
     public ResponseEntity<ListFollowedResponseDTO> listFollowed (@PathVariable int userId, @RequestParam(required = false, defaultValue = "name_asc") String order) throws InexistentUserException, InexistentNameOrderException {
         return new ResponseEntity<>(userService.listFollowed(userId, order), HttpStatus.OK);
     }
+
+    /**
+     * Unfollows a user
+     *
+     * @param userId the userId of the unfollower user
+     * @param userIdToUnfollow the userId of the user to unfollow
+     * @return the status and message of success in unfollow request
+     * @throws InexistentUserException if the user does not exist in th local repository
+     * @throws InexistentFollowerException if the unfollower user does not follow the user to unfollow
+     */
 
     @PostMapping("/{userId}/unfollow/{userIdToUnfollow}")
     public ResponseEntity<FollowSellerResponseDTO> unfollowSeller (@PathVariable int userId, @PathVariable int userIdToUnfollow) throws InexistentUserException, InexistentFollowerException {
