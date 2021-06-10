d9e1a8e25485d03feffc7e15e8a51291d56d38b2
Perez Stefania <stefania.perez@merccadolibre.com.co>
2021-06-09 21:16:26 -0500

    Avance 4 dia 2

--- 10_desafio1/demo 2/src/main/java/com/example/demo/socialmeli/service/PublicationService.java
+++ 10_desafio1/demo 2/src/main/java/com/example/demo/socialmeli/service/PublicationService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 @Service
-public class PublicationService implements IPublicationService{
+public class PublicationService implements IPublicationService {
     private final UserRepository userRepository;
 
     public PublicationService(UserRepository userRepository) {
@@ -19,17 +19,17 @@ public class PublicationService implements IPublicationService{
 
     @Override
     public void addNewPost(PublicationRequestDTO publicationRequestDTO) {
-        int id= publicationRequestDTO.getUserId();
+        int id = publicationRequestDTO.getUserId();
         User user = userRepository.getUserById(id);
         user.getPosts().add(publicationRequestDTO.getId_post());
         userRepository.refreshUser(user);
-        Publication publication =  new Publication();
+        Publication publication = new Publication();
         publication.setCategory(publicationRequestDTO.getCategory());
         publication.setId_post(publicationRequestDTO.getId_post());
         publication.setDate(publicationRequestDTO.getDate());
         publication.setDetail(publicationRequestDTO.getDetail());
         publication.setPrice(publicationRequestDTO.getPrice());
-         userRepository.refreshPublications(publication);
+        userRepository.refreshPublications(publication);
     }
 
     @Override
@@ -37,34 +37,32 @@ public class PublicationService implements IPublicationService{
         User user = userRepository.getUserById(id);
         List<Publication> publications = userRepository.getAllPublication();
         ArrayList<Publication> arrayList = new ArrayList<>();
-        for (int i =0;i<user.getPosts().size();i++) {
+        for (int i = 0; i < user.getPosts().size(); i++) {
             int idPost = user.getPosts().get(i);
-            Optional<Publication> item = publications.stream().filter(publication -> publication.getId_post()==idPost).findFirst();
-            if (item.isPresent()){
+            Optional<Publication> item = publications.stream().filter(publication -> publication.getId_post() == idPost).findFirst();
+            if (item.isPresent()) {
                 arrayList.add(item.get());
             }
         }
         PublicationListDTO publicationListDTO = new PublicationListDTO();
         publicationListDTO.setUserId(id);
-        if (order!=null){
-            if (order.equals("date_asc"))
-                arrayList.sort((a,b)->a.getDate().compareTo(b.getDate()));
-            else if (order.equals("date_desc"))
-                arrayList.sort((a,b)->b.getDate().compareTo(a.getDate()));
-            publicationListDTO.setPosts(arrayList);
-        }else {
-            arrayList.sort((a, b) -> b.getDate().compareTo(a.getDate()));
-            Calendar date = Calendar.getInstance();
-            date.add(Calendar.DAY_OF_YEAR, -14);
-            Date finallyDate = date.getTime();
-            ArrayList<Publication> arrayList1 = new ArrayList<>();
-            for (int k = 0; k < arrayList.size(); k++) {
-                if (finallyDate.before(arrayList.get(k).getDate())) {
-                    arrayList1.add(arrayList.get(k));
-                }
+        arrayList.sort((a, b) -> b.getDate().compareTo(a.getDate()));
+        Calendar date = Calendar.getInstance();
+        date.add(Calendar.DAY_OF_YEAR, -14);
+        Date finallyDate = date.getTime();
+        ArrayList<Publication> arrayList1 = new ArrayList<>();
+        for (int k = 0; k < arrayList.size(); k++) {
+            if (finallyDate.before(arrayList.get(k).getDate())) {
+                arrayList1.add(arrayList.get(k));
             }
-            publicationListDTO.setPosts(arrayList1);
         }
+        if (order != null) {
+            if (order.equals("date_asc"))
+                arrayList1.sort((a, b) -> a.getDate().compareTo(b.getDate()));
+            else if (order.equals("date_desc"))
+                arrayList1.sort((a, b) -> b.getDate().compareTo(a.getDate()));
+        }
+        publicationListDTO.setPosts(arrayList1);
         return publicationListDTO;
     }
 }
