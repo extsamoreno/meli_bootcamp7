60f06619643a3ec226c3809db5e85e14253637b4
Gustavo Sosa <gustavo.sosa@mercadolibre.com>
2021-06-11 15:07:32 -0300

    feat:added data entry validations

--- 11_testing1/obtener-diploma/src/main/java/com/digitalhouse/obtenerdiploma/dto/StudentDTO.java
+++ 11_testing1/obtener-diploma/src/main/java/com/digitalhouse/obtenerdiploma/dto/StudentDTO.java
@@ -1,10 +1,21 @@
 package com.digitalhouse.obtenerdiploma.dto;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public class StudentDTO {
+
+    @NotBlank(message = "El nombre del alumno no puede estar vacío.")
+    @Pattern(regexp = "([A-Z][a-zäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙ]+[\\p{Space}]*)*", message = "El nombre del alumno debe comenzar con mayúscula.")
+    @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
     private String studentName;
-    private List<SubjectDTO> subjects;
+
+    @NotEmpty(message = "La lista no puede ser vacía.")
+    private List<@Valid SubjectDTO> subjects;
 
     public StudentDTO() {
     }
--- 11_testing1/obtener-diploma/src/main/java/com/digitalhouse/obtenerdiploma/dto/SubjectDTO.java
+++ 11_testing1/obtener-diploma/src/main/java/com/digitalhouse/obtenerdiploma/dto/SubjectDTO.java
@@ -1,23 +1,33 @@
 package com.digitalhouse.obtenerdiploma.dto;
 
+import javax.validation.constraints.*;
+
 public class SubjectDTO {
-  private String name;
-  private Integer score;
 
-  public String getName() {
-    return name;
-  }
+    @NotBlank(message = "El nombre de la materia no puede estar vacío.")
+    @Pattern(regexp = "([A-Z][a-zäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙ]+[\\p{Space}]*)*", message = "El nombre de la materia debe comenzar con mayúscula.")
+    @Size(max = 30, message = "La longitud del nombre no puede superar los 30 caracteres.")
+    private String name;
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    @NotNull(message = "La nota no puede estar vacía.")
+    @DecimalMax(value = "10.0",message = "La máxima nota es 10.0.")
+    @DecimalMin(value = "0.0",message = "La mínima nota es 0.0.")
+    private double score;
 
-  public Integer getScore() {
-    return score;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setScore(Integer score) {
-    this.score = score;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
 
 }
