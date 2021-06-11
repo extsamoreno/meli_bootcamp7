3c8881f86fa0c060ada79209057bbc85d656fa13
alaranda <alexis.aranda@mercadolibre.cl>
2021-06-11 12:05:24 -0400

    refactor: change empty string validation

--- 11_testing1/diploms/src/main/java/com/example/diploms/Services/DTOs/StudentDTO.java
+++ 11_testing1/diploms/src/main/java/com/example/diploms/Services/DTOs/StudentDTO.java
@@ -6,20 +6,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class StudentDTO {
 
-    @NotEmpty(message = "El nombre del alumno no puede estar vacío.")
+    @NotBlank(message = "El nombre del alumno no puede estar vacío.")
     @Pattern(regexp = "^[A-Z][a-zA-ZÀ-ÿ\\u00f1\\u00d1]+(\\s*[a-zA-ZÀ-ÿ\\u00f1\\u00d1]*)*[a-zA-ZÀ-ÿ\\u00f1\\u00d1]*$", message = "El nombre del alumno comenzar con mayúscula.")
     @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
     private String studentName;
-
 
     @NotEmpty(message = "La lista no puede ser vacía.")
     private List<@Valid SubjectDTO> subjects;
--- 11_testing1/diploms/src/main/java/com/example/diploms/Services/DTOs/SubjectDTO.java
+++ 11_testing1/diploms/src/main/java/com/example/diploms/Services/DTOs/SubjectDTO.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.*;
 @Setter @Getter @AllArgsConstructor @NoArgsConstructor
 public class SubjectDTO {
 
-    @NotEmpty(message = "El nombre de la materia no puede estar vacío")
+    @NotBlank(message = "El nombre de la materia no puede estar vacío")
     @Pattern(regexp = "^[A-Z][a-zA-ZÀ-ÿ\\u00f1\\u00d1]+(\\s*[a-zA-ZÀ-ÿ\\u00f1\\u00d1]*)*[a-zA-ZÀ-ÿ\\u00f1\\u00d1]*$", message = "El nombre de la materia debe comenzar con mayúscula")
     @Size (max = 50, message = "La longitud del nombre no puede superar los 30 caracteres")
     private String name;
