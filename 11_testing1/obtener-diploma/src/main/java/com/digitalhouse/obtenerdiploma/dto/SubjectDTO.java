53bb1a7be978c0dfb2c2c074cc7d708686c05907
italoNovoaMel <italo.cufinonovoa@mercadolibre.com.co>
2021-06-11 09:45:11 -0500

    start

---
+++ 11_testing1/obtener-diploma/src/main/java/com/digitalhouse/obtenerdiploma/dto/SubjectDTO.java
@@ -0,0 +1,23 @@
+package com.digitalhouse.obtenerdiploma.dto;
+
+public class SubjectDTO {
+  private String subject;
+  private Integer note;
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  public Integer getNote() {
+    return note;
+  }
+
+  public void setNote(Integer note) {
+    this.note = note;
+  }
+
+}
