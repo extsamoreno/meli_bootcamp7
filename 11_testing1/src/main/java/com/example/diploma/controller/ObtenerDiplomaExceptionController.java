f15da1780dbe21ddb0afd08ef293544af364a3a5
Macarena Agostina Pomerantz <mpomerantz@ar0c02ff0guml85.ml.com>
2021-06-11 12:45:34 -0300

    Fix Clase 9 Practica TM

--- 11_testing1/src/main/java/com/example/diploma/controller/ObtenerDiplomaExceptionController.java
+++ 11_testing1/src/main/java/com/example/diploma/controller/ObtenerDiplomaExceptionController.java
@@ -3,18 +3,15 @@ package com.example.diploma.controller;
 import com.example.diploma.dto.ErrorDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class ObtenerDiplomaExceptionController {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    /*@ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
@@ -23,5 +20,12 @@ public class ObtenerDiplomaExceptionController {
             errors.put(fieldName, errorMessage);
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }*/
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        return new ResponseEntity<>(
+                new ErrorDTO(e.getBindingResult().getAllErrors(), e.getClass().getSimpleName()),
+                HttpStatus.OK);
     }
 }
--- 11_testing1/src/main/java/com/example/diploma/dto/ErrorDTO.java
+++ 11_testing1/src/main/java/com/example/diploma/dto/ErrorDTO.java
@@ -4,10 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+
+import java.util.HashMap;
+import java.util.List;
 
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
 public class ErrorDTO {
     private String error;
-    private String message;
+    private HashMap<String, String> messageErrors;
+
+    public ErrorDTO(List<ObjectError> errors, String errorClass){
+        this.messageErrors = new HashMap<>();
+        errors.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            messageErrors.put(fieldName, errorMessage);
+        });
+        this.error = errorClass;
+    }
 }
