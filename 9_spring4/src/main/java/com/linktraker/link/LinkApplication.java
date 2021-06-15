6b5dcbd0cbc93d99ab9d9802839890133b9467d7
luzvictoria-mp <luz.victoria@mercadolibre.com>
2021-06-14 21:54:28 -0300

    Practica testing

---
+++ 9_spring4/src/main/java/com/linktraker/link/LinkApplication.java
@@ -0,0 +1,18 @@
+package com.linktraker.link;
+
+import com.linktraker.link.Model.LinkModel;
+import com.linktraker.link.Repository.LinkRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkApplication.class, args);
+    }
+
+
+
+
+}
