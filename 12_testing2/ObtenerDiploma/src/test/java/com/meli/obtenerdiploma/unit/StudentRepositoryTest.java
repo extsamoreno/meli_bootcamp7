daae7d54e5941ff4b147c252b9f29ab242006b84
Nahuel Nicolas Tosi Brandi <ntosi@ar0c02ff0mfml85.fibertel.com.ar>
2021-06-15 15:37:36 -0300

    exercises tests

---
+++ 12_testing2/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/unit/StudentRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.meli.obtenerdiploma.unit;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import com.meli.obtenerdiploma.repository.StudentDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class StudentRepositoryTest {
+
+    @Test
+    public void studentSaveHappyPath(){
+        // Arrange
+        SubjectDTO subject = new SubjectDTO( "Matematica", 9.00);
+        List<SubjectDTO> subjects = new ArrayList<SubjectDTO>();
+        subjects.add(subject);
+        StudentDTO stu =  new StudentDTO(1L,"Juan", "hola", 9.9, subjects);
+        StudentDAO stuDAO = new StudentDAO();
+        stuDAO.save(stu);
+
+        // act
+        boolean receive = stuDAO.exists(stu);
+
+        // Assert
+        Assertions.assertTrue(receive);
+    }
+
+    @Test
+    public void studentDeleteStudentHappyPath(){
+        // Arrange
+        SubjectDTO subject = new SubjectDTO( "Matematica", 9.00);
+        List<SubjectDTO> subjects = new ArrayList<SubjectDTO>();
+        subjects.add(subject);
+        long studentId = 1L;
+        StudentDTO stu =  new StudentDTO(studentId,"Juan", "hola", 9.9, subjects);
+        StudentDAO stuDAO = new StudentDAO();
+        stuDAO.save(stu);
+
+        // act
+        boolean receive = stuDAO.delete(stu.getId());
+
+        // Assert
+        Assertions.assertTrue(receive);
+    }
+
+    @Test
+    public void studentDeleteStudentStudentDoNotExit(){
+        // Arrange
+        SubjectDTO subject = new SubjectDTO( "Matematica", 9.00);
+        List<SubjectDTO> subjects = new ArrayList<SubjectDTO>();
+        subjects.add(subject);
+        long studentId = 1L;
+        StudentDTO stu =  new StudentDTO(studentId,"Juan", "hola", 9.9, subjects);
+//        StudentDTO stu2 =  new StudentDTO(2L,"Juan", "hola", 9.9, subjects);
+        StudentDAO stuDAO = new StudentDAO();
+        stuDAO.save(stu);
+
+        // act
+        boolean receive = stuDAO.delete(5L);
+
+        // Assert
+        Assertions.assertFalse(receive);
+    }
+}
---
+++ 12_testing2/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/unit/StudentServiceTests.java
@@ -0,0 +1,93 @@
+package com.meli.obtenerdiploma.unit;
+
+import com.meli.obtenerdiploma.exception.StudentNotFoundException;
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import com.meli.obtenerdiploma.repository.IStudentDAO;
+import com.meli.obtenerdiploma.service.ObtenerDiplomaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class StudentServiceTests {
+
+
+    @Mock
+    IStudentDAO iStudentDAO;
+
+    @InjectMocks
+    ObtenerDiplomaService obtenerDiplomaService;
+
+    @Test
+    public void ObtenerDiplomaHappyPathCongratulation(){
+        // Arrange
+        Long studentId = 1L;
+        SubjectDTO subject = new SubjectDTO( "Matematica", 9.0);
+        SubjectDTO subject2 = new SubjectDTO( "Lengua", 10.0);
+
+        List<SubjectDTO> subjects = new ArrayList<SubjectDTO>();
+        subjects.add(subject);
+        subjects.add(subject2);
+
+        StudentDTO student =  new StudentDTO(studentId,"Juan", "hola", 0.0, subjects);
+
+        StudentDTO expected = new StudentDTO(studentId,"Juan", "hola", 0.0, subjects);
+        expected.setMessage("El alumno Juan ha obtenido un promedio de 9.5. Felicitaciones!" );
+        expected.setAverageScore(9.5 );
+        when(iStudentDAO.findById(studentId)).thenReturn(student);
+
+        // Act
+        StudentDTO received = obtenerDiplomaService.analyzeScores(studentId);
+
+        // Assert
+        verify(iStudentDAO, times(1)).findById(studentId);
+        Assertions.assertEquals(expected, received);
+
+    }
+
+    @Test
+    public void ObtenerDiplomaHappyPathPuedeMejorar(){
+        // Arrange
+        Long studentId = 1L;
+        SubjectDTO subject = new SubjectDTO( "Matematica", 8.0);
+        SubjectDTO subject2 = new SubjectDTO( "Lengua", 9.0);
+        List<SubjectDTO> subjects = new ArrayList<SubjectDTO>();
+        subjects.add(subject);
+        subjects.add(subject2);
+        StudentDTO student =  new StudentDTO(studentId,"Juan", "hola", 0.0, subjects);
+        StudentDTO expected = new StudentDTO(studentId,"Juan", "hola", 0.0, subjects);
+        expected.setMessage("El alumno Juan ha obtenido un promedio de 8.5. Puedes mejorar." );
+        expected.setAverageScore(8.5 );
+        when(iStudentDAO.findById(studentId)).thenReturn(student);
+
+        // Act
+        StudentDTO received = obtenerDiplomaService.analyzeScores(studentId);
+
+        // Assert
+        verify(iStudentDAO, times(1)).findById(studentId);
+        Assertions.assertEquals(expected, received);
+
+    }
+
+    @Test
+    public void ObtenerDiplomaEmptyList(){
+        // Arrange
+        Long studentId = 100L;
+        when(iStudentDAO.findById(studentId)).thenThrow(new StudentNotFoundException(studentId));
+
+        // Act // Assert
+        assertThrows(StudentNotFoundException.class, () -> obtenerDiplomaService.analyzeScores(studentId));
+
+    }
+}
