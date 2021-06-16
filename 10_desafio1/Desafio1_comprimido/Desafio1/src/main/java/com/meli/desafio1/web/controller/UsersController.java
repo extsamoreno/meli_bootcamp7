4b664bd72607095329fa8cae84d4b61dfe002fab
daprieto-melidev <diego.prieto@mercadolibre.com.co>
2021-06-16 16:20:45 -0500

    reuploaded Challenge1 with stable version

--- 10_desafio1/Desafio1_comprimido/Desafio1/src/main/java/com/meli/desafio1/web/controller/UsersController.java
+++ 10_desafio1/Desafio1_comprimido/Desafio1/src/main/java/com/meli/desafio1/web/controller/UsersController.java
@@ -42,6 +42,7 @@ public class UsersController {
 
     @PostMapping("/{userId}/unfollow/{userIdToUnfollow}")
     public ResponseEntity<String> unFollowResponse(@PathVariable("userId") int userId, @PathVariable("userIdToUnfollow") int userIdToUnfollow) throws UserException {
+
         iUserService.unFollow(userId,userIdToUnfollow);
         return new ResponseEntity<>("todo OK", HttpStatus.OK);
     }
