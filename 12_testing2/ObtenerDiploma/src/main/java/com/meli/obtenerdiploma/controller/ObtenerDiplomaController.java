1d6699fc1a665a1a0c5ad39c284b5718365c6860
Mariano Razetto <mariano.razetto@mercadolibre.com>
2021-06-16 10:16:40 -0300

    testing 3

--- 12_testing2/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaController.java
+++ 12_testing2/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaController.java
@@ -16,6 +16,7 @@ public class ObtenerDiplomaController {
 
     @GetMapping("/analyzeScores/{studentId}")
     public StudentDTO analyzeScores(@PathVariable Long studentId) {
+
         return service.analyzeScores(studentId);
     }
 }
