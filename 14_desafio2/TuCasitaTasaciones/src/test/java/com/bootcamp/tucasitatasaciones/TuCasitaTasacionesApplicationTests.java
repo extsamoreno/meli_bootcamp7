ec20d69da4ece2d81c848094330808faa89f64d8
Geronimo <geronimo.bazan@mercadolibre.com>
2021-06-16 15:43:41 -0300

    Starting the challenge

---
+++ 14_desafio2/TuCasitaTasaciones/src/test/java/com/bootcamp/tucasitatasaciones/TuCasitaTasacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.tucasitatasaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuCasitaTasacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
