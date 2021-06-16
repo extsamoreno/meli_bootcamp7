8412a94901a5f35de37f53ef9ef65072e4e2481c
nian-meli <nicoll.mejia@mercadolibre.com.co>
2021-06-16 13:37:12 -0500

    Create proyect

---
+++ 14_desafio2/src/main/java/com/desafrio2/demo/Desafrio2Application.java
@@ -0,0 +1,13 @@
+package com.desafrio2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafrio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafrio2Application.class, args);
+	}
+
+}
---
+++ 14_desafio2/src/test/java/com/desafrio2/demo/Desafrio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafrio2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafrio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
