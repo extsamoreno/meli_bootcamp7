5b8beadd83616447f70f4d9738510847fc203483
olarte9711 <oscar.ovargas@mercadolibre.com.co>
2021-06-16 13:36:07 -0500

    Initializing challenge 2

---
+++ 14_desafio2/ChallengeTwo/src/main/java/com/example/ChallengeTwo/ChallengeTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChallengeTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTwoApplication.class, args);
+	}
+
+}
---
+++ 14_desafio2/ChallengeTwo/src/test/java/com/example/ChallengeTwo/ChallengeTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChallengeTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
