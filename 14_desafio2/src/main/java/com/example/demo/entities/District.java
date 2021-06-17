b52a9103581bbe64a9e6516599ab41fa71681771
Camila Taeger <camila.taeger@mercadolibre.com>
2021-06-17 01:42:04 -0300

    Adding Validates

--- 14_desafio2/src/main/java/com/example/demo/entities/District.java
+++ 14_desafio2/src/main/java/com/example/demo/entities/District.java
@@ -3,6 +3,7 @@ package com.example.demo.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.validation.constraints.*;
 
 @Data
 @AllArgsConstructor
@@ -10,6 +11,13 @@ import lombok.NoArgsConstructor;
 public class District {
 
     private int id;
+
+    @NotBlank(message = "District can't be empty")
+    @Size(max = 45, message = "The length of the neighborhood cannot exceed 45 characters")
     private String name;
-    private double price;
+
+    @NotNull(message = "Price can't be empty")
+    @DecimalMax(value="4000.0", message = "The maximum price allowed per square meter cannot exceed 4000 U$S")
+    @Positive(message = "The price must be positive")
+    private Double price;
 }
--- 14_desafio2/src/main/java/com/example/demo/entities/Property.java
+++ 14_desafio2/src/main/java/com/example/demo/entities/Property.java
@@ -3,7 +3,8 @@ package com.example.demo.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.util.List;
 
 
@@ -13,6 +14,10 @@ import java.util.List;
 public class Property {
 
     private int id;
+
+    @Pattern(regexp="([A-Z]|[0-9])[\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "The name of the property must begin with a capital letter")
+    @NotBlank(message = "The name can't be empty")
+    @Size(max = 30, message = "The length of the name cannot exceed 30 characters")
     private String name;
     private District district;
     private List<Environment> environments;
