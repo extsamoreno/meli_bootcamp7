6fc4a9aaa121b621544b9bb6d04f9e1bb37a9785
Michael Andrey Rivera Castro <mirivera@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-06-17 14:53:44 -0500

    Adding postman collection

---
+++ 14_desafio2/TuCasitaAPI/src/test/java/controller/PropertyControllerTest.java
@@ -0,0 +1,35 @@
+package controller;
+
+import com.meli.tucasita.controller.PropertyController;
+import com.meli.tucasita.exception.PropertyNotFoundException;
+import com.meli.tucasita.service.IPropertyService;
+import com.meli.tucasita.service.dto.PropertyWithM2DTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+public class PropertyControllerTest {
+
+    @Mock
+    IPropertyService iPropertyService;
+
+    @InjectMocks
+    PropertyController propertyController;
+
+    @Test
+    public void correctTotalMetersOfAnProperty() throws PropertyNotFoundException {
+        // Arrange
+        String propertyName= "Propiedad prueba 1";
+
+        Mockito.when(iPropertyService.getTotalM2(propertyName)).thenReturn();
+        // Act
+        ResponseEntity<PropertyWithM2DTO> response= propertyController.getTotalM2(propertyName);
+        // Assert
+    }
+
+}
