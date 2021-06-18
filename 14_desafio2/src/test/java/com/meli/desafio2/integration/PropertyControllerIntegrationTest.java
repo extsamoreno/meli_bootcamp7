6d332e10b5118a8b3a3dbf3485c51771db79e598
MatiasBossaML <matias.bossa@mercadolibre.com>
2021-06-18 15:46:51 -0300

    Integration

--- 14_desafio2/src/test/java/com/meli/desafio2/integration/PropertyControllerIntegrationTest.java
+++ 14_desafio2/src/test/java/com/meli/desafio2/integration/PropertyControllerIntegrationTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -54,7 +53,6 @@ public class PropertyControllerIntegrationTest {
 
     @Test
     public void registerPropertyNullPayload() throws Exception{
-
 
         this.mockMvc.perform(MockMvcRequestBuilders.post("/property/save"))
                 .andDo(print())
