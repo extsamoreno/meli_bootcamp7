bb7687b7d233e6aa0bea50e9b15ca8e3be3145f7
Michael Andrey Rivera Castro <mirivera@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-06-18 11:52:35 -0500

    Configuring run characteristics and adding some other service test for unrequested service unit test

--- 14_desafio2/TuCasitaAPI/src/test/java/com/meli/tucasita/service/PropertyServiceTest.java
+++ 14_desafio2/TuCasitaAPI/src/test/java/com/meli/tucasita/service/PropertyServiceTest.java
@@ -1,15 +1,13 @@
 package com.meli.tucasita.service;
 
+import com.meli.tucasita.exception.DistrictNotFoundException;
 import com.meli.tucasita.exception.PropertyNotFoundException;
 import com.meli.tucasita.model.District;
 import com.meli.tucasita.model.Environment;
 import com.meli.tucasita.model.Property;
 import com.meli.tucasita.repository.IDistrictRepository;
 import com.meli.tucasita.repository.IPropertyRepository;
-import com.meli.tucasita.service.dto.EnvironmentResponseDTO;
-import com.meli.tucasita.service.dto.PropertyWithBiggerEnvDTO;
-import com.meli.tucasita.service.dto.PropertyWithEnvAndMetersDTO;
-import com.meli.tucasita.service.dto.PropertyWithM2DTO;
+import com.meli.tucasita.service.dto.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +15,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -135,6 +134,50 @@ public class PropertyServiceTest {
                 new EnvironmentResponseDTO("Habitacion 2",361)
         )));
         Assertions.assertEquals(expected,response);
+    }
+
+    @Test
+    public void matchPriceWithAnExistentProperty() throws PropertyNotFoundException {
+        // Arrange
+        String propertyName= "Propiedad prueba 1";
+        Property property= new Property(propertyName,new District("Fontibon",2000),new ArrayList<>(Arrays.asList(
+                new Environment("Baño",20,15),
+                new Environment("Cocina",24,30),
+                new Environment("Habitacion 1",15,19),
+                new Environment("Habitacion 2",19,19)
+        )));
+        Mockito.when(iPropertyRepository.getPropertyByName(propertyName)).thenReturn(property);
+        // Act
+        PropertyWithPriceDTO received= propertyService.getPrice(propertyName);
+        // Assert
+        PropertyWithPriceDTO expected= new PropertyWithPriceDTO(propertyName,3332000);
+        Assertions.assertEquals(expected,received);
+    }
+
+    @Test
+    public void addANewPropertyWithExistentDistrict() throws DistrictNotFoundException {
+        // Arrange
+        String propertyName= "Propiedad prueba 1";
+        String districtName= "Fontibon";
+        PropertyDTORequest propertyRequest= new PropertyDTORequest(propertyName,districtName,new ArrayList<>(Arrays.asList(
+                new Environment("Baño",20,15),
+                new Environment("Cocina",24,30),
+                new Environment("Habitacion 1",15,19),
+                new Environment("Habitacion 2",19,19)
+        )));
+        Property property= new Property(propertyName,new District(districtName,2000),new ArrayList<>(Arrays.asList(
+                new Environment("Baño",20,15),
+                new Environment("Cocina",24,30),
+                new Environment("Habitacion 1",15,19),
+                new Environment("Habitacion 2",19,19)
+        )));
+        District district= new District(districtName,2000);
+        Mockito.when(iDistrictRepository.getDistrictByName(districtName)).thenReturn(district);
+        Mockito.when(iPropertyRepository.addNewProperty(property)).thenReturn(HttpStatus.CREATED);
+        // Act
+        HttpStatus received= propertyService.addNewProperty(propertyRequest);
+        // Assert
+        Assertions.assertEquals(HttpStatus.CREATED,received);
     }
 
 }
