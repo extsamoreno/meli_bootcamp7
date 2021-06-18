426b6f6ced30a3c3f2d3eee193513e175bcd0518
agustin-amartinez <agustin.amartinez@mercadolibre.com>
2021-06-18 11:58:15 -0300

    add README.md

---
+++ 14_desafio2/TuCasita/src/README.md
@@ -0,0 +1,20 @@
+# Tu Casita
+
+
+
+## EndPoints used
+
+
+#### GET localhost:8080/houses/getmeter2/{idHouse} 
+  - returns the largest ambient of a house
+
+#### GET http://localhost:8080/houses/getprice/{idHOuse}
+  - returns price of a house
+
+#### GET http://localhost:8080/houses/getmaxambient/{idHouse}
+  -returns the largest ambient of a house
+
+#### GET http://localhost:8080/houses/{idHouse}/ambient/{Name_ambient}/getmaxambient
+  - returns the square meters an ambient
+
+
--- 14_desafio2/TuCasita/src/main/java/com/meli/tuCasita/controller/HousesController.java
+++ 14_desafio2/TuCasita/src/main/java/com/meli/tuCasita/controller/HousesController.java
@@ -18,24 +18,50 @@ public class HousesController {
     @Autowired
     IHouseService iHouseService;
 
+    /**
+     *
+     * @param id by house
+     * @return returns square meters of a house
+     * @throws HouseNotFoundException
+     */
     @GetMapping("/getmeter2/{id}")
     public ResponseEntity<?> getMeter2(@Valid @PathVariable Long id) throws HouseNotFoundException {
         Double value = this.iHouseService.getMeter2(id);
         return ResponseEntity.ok(value);
     }
 
+    /**
+     *
+     * @param id by house
+     * @return returns price of a house
+     * @throws HouseNotFoundException
+     */
     @GetMapping("/getprice/{id}")
     public ResponseEntity<?> getPrice(@Valid @PathVariable Long id) throws HouseNotFoundException {
         Double value = this.iHouseService.getPrice(id);
         return ResponseEntity.ok(value);
     }
 
+
+    /**
+     *
+     * @param id by house
+     * @return returns the largest ambient of a house
+     * @throws HouseNotFoundException
+     */
     @GetMapping("/getmaxambient/{id}")
     public ResponseEntity<?> getMaxAmbient(@Valid @PathVariable Long id) throws HouseNotFoundException{
         AmbientDTO value = this.iHouseService.getMaxAmbient(id);
         return ResponseEntity.ok(value);
     }
 
+    /**
+     *
+     * @param idHouse by house
+     * @param nameAmbient by ambient
+     * @return returns the square meters an ambient
+     * @throws HouseNotFoundException
+     */
     @GetMapping("/{idHouse}/ambient/{nameAmbient}/getmaxambient")
     public ResponseEntity<?> getmaxambient(@Valid @PathVariable Long idHouse,@PathVariable String nameAmbient) throws HouseNotFoundException {
         HashMap value = this.iHouseService.getmaxambientforambient(idHouse, nameAmbient);
