e00985917bdf6aa965b18375259ee873dcf9e579
sgalfre <sofia.galfre@mercadolibre.com>
2021-06-23 13:20:41 -0300

    Update queries.sql

--- 16_sql2/TM/queries.sql
+++ 16_sql2/TM/queries.sql
@@ -7,15 +7,15 @@ FROM series JOIN genres AS g ON genre_id = g.id;
 
 SELECT title, first_name, last_name
 FROM episodes 
-	JOIN actor_episode ON episodes.id = episode_id 
-	JOIN actors ON actor_episode.id= actors.id;
+JOIN actor_episode ON episodes.id = episode_id 
+JOIN actors ON actor_episode.id= actors.id;
 
 
 -- 3. Mostrar el título de todas las series y el total de temporadas que tiene
 -- cada una de ellas.
 SELECT series.title AS "Serie", COUNT(*) AS "Cantidad de temporadas"
 FROM series 
-	JOIN seasons AS se ON series.id = se.serie_id
+JOIN seasons AS se ON series.id = se.serie_id
 GROUP BY series.title;
 
 
@@ -23,7 +23,7 @@ GROUP BY series.title;
 -- por cada uno,  siempre que sea mayor o igual a 3.
 SELECT gs.name, COUNT(*) AS "Cantidad de películas"
 FROM genres AS gs 
-	JOIN movies AS m ON m.genre_id = gs.id
+JOIN movies AS m ON m.genre_id = gs.id
 GROUP BY gs.name;
 
 
@@ -31,7 +31,7 @@ GROUP BY gs.name;
 -- las películas  de la guerra de las galaxias y que estos no se repitan.
 SELECT first_name, last_name
 FROM actor_movie AS am 
-	JOIN movies AS m ON am.movie_id = m.id 
-    JOIN actors AS a ON a.id = am.actor_id
+JOIN movies AS m ON am.movie_id = m.id 
+JOIN actors AS a ON a.id = am.actor_id
 WHERE m.title LIKE "La guerra de las galaxias%"
 GROUP BY first_name, last_name;
