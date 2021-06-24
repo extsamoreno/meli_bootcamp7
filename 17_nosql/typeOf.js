fe32fd5a996d0d50fae8bed539d6f6449953bc86
Macarena Agostina Pomerantz <mpomerantz@ar0c02ff0guml85.fibertel.com.ar>
2021-06-24 17:05:58 -0300

    Practica Clase 14

---
+++ 17_nosql/typeOf.js
@@ -0,0 +1,4 @@
+//var schematodo = db.collection_name.findOne();
+
+//for (var key in schematodo) { print (key, typeof key) ; }
+[db.restaurants.findOne()].forEach( function(my_doc) { for (var key in my_doc) { print(key + ': ' + typeof my_doc[key]) } } )
