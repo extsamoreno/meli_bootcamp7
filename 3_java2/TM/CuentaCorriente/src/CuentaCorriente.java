030fdff772028e4d34ca3f9a359ed1eef6b4fcc1
marcos rausch <marcos.rausch@mercadolibre.com>
2021-06-26 03:49:51 -0300

    fixed copier constructor

--- 3_java2/TM/CuentaCorriente/src/CuentaCorriente.java
+++ 3_java2/TM/CuentaCorriente/src/CuentaCorriente.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class CuentaCorriente {
+public class CuentaCorriente {
+    private double saldo;
+
+    public CuentaCorriente () {
+
+    }
+
+    // se asegura de que el saldo sea siempre mayor a cero
+    public CuentaCorriente (double saldoInicial) {
+        if (saldoInicial > 0.0) {
+            saldo = saldoInicial;
+        }
+    }
+
+    // constructor copiador
+    public CuentaCorriente (CuentaCorriente cuentaCorriente) {
+        this.saldo = cuentaCorriente.obtenerSaldo();
+    }
+
+
+    // métodos
+    public void ingreso (double monto) {
+        saldo += monto;
+    }
+
+    public double egreso (double monto) {
+        return saldo -= monto;
+    }
+
+    public void reintegro (double monto) {
+        ingreso(monto);
+    }
+
+    public void transferencia (double monto, CuentaCorriente cuentaDestino) {
+        saldo -= monto;
+        cuentaDestino.saldo += monto;
+    }
+
+    // métodos de acceso
+    public double obtenerSaldo () {
+        return saldo;
+    }
+
+    public void establecerSaldo (int saldo) {
+        this.saldo = saldo;
+    }
+
 }
