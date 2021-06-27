51e3372dddc3e7e4258739cf2cc53fdbc0076a75
marcos rausch <marcos.rausch@mercadolibre.com>
2021-06-26 23:22:47 -0300

    Java 2, TT, e 2

--- 3_java2/TT/FiguraGeometrica/src/Util.java
+++ 3_java2/TT/FiguraGeometrica/src/Util.java
@@ -7,4 +7,18 @@ public class Util  {
         int avg = sum / arr.length;
         return avg;
     }
+    /* FiguraGeométrica no puede instanciarse, por ser abstracta;
+        pero sí se puede obtener un arreglo de objetos de una clase
+        que herede de ella.
+    */
+
+    public static void main(String[] args) {
+        FiguraGeometrica arr[] = new FiguraGeometrica[3];
+        arr[0] = new Circulo(10);
+        arr[1] = new Rectangulo(5, 3);
+        arr[2] = new Triangulo(4, 6);
+
+        double areaPromedio = areaPromedio(arr);
+        System.out.println("El área promedio de todas las figuras del arreglo es de " + areaPromedio);
+    }
 }
