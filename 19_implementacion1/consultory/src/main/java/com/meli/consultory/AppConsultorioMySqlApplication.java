91beb7e6604e3230544507022acc3f063c17a13b
Diego Pinto <diego.pinto@mercadolibre.com.co>
2021-06-29 18:15:44 -0500

    Creaciòn de nuevo proyecto y estructura del mismo

---
+++ 19_implementacion1/consultory/src/main/java/com/meli/consultory/AppConsultorioMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.meli.consultory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConsultorioMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConsultorioMySqlApplication.class, args);
+	}
+
+}
