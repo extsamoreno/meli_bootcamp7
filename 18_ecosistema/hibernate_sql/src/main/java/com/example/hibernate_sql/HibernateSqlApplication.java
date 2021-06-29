54117158f572f4f895dc839e2be868d7ccb5c732
tbudassi <tomas.budassi@mercadolibre.com>
2021-06-29 19:57:41 -0300

    testing sql implements

---
+++ 18_ecosistema/hibernate_sql/src/main/java/com/example/hibernate_sql/HibernateSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateSqlApplication.class, args);
+    }
+
+}
---
+++ 18_ecosistema/hibernate_sql/src/test/java/com/example/hibernate_sql/HibernateSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
