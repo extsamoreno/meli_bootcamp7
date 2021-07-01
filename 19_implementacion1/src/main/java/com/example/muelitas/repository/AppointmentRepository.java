cabdbba66bbd2dde2de7b4615f52b799e8949d49
Macarena Agostina Pomerantz <mpomerantz@ar0c02ff0guml85.fibertel.com.ar>
2021-07-01 02:23:14 -0300

    Practica Clase 17 - HQL Queries

--- 19_implementacion1/src/main/java/com/example/muelitas/repository/AppointmentRepository.java
+++ 19_implementacion1/src/main/java/com/example/muelitas/repository/AppointmentRepository.java
@@ -12,4 +12,22 @@ import java.util.List;
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
     @Query("SELECT patient.id, patient.name, patient.medicalInsurance FROM Patient patient JOIN Appointment appointment ON appointment.patient.id = patient.id WHERE appointment.date BETWEEN :dateStart AND :dateEnd")
     List findAllPatientsByDate(@Param("dateStart") LocalDateTime dateStart, @Param("dateEnd") LocalDateTime dateEnd);
+
+    @Query("SELECT professional.id, professional.name, COUNT(*) FROM Professional professional JOIN Appointment appointment ON appointment.professional.id = professional.id GROUP BY professional.id")
+    List findAllProfessionalsWithMoreThanTwoAppointments();
+
+    @Query("SELECT appointment.id FROM Appointment appointment WHERE appointment.status LIKE 'Finalizado'")
+    List findAllAppointmentsWithStateFinalizado();
+
+    @Query("SELECT appointment.id FROM Appointment appointment WHERE appointment.status LIKE 'Finalizado' AND appointment.date BETWEEN :dateStart AND :dateEnd")
+    List findAllAppointmentsWithStatePendienteAndADay(@Param("dateStart") LocalDateTime dateStart, @Param("dateEnd") LocalDateTime dateEnd);
+
+    @Query("SELECT appointment.id FROM Professional professional JOIN Appointment appointment ON appointment.professional.id = professional.id WHERE professional.id = :idProfessional")
+    List findAllProfessionalAppointments(@Param("idProfessional") Long idProfessional);
+
+    @Query("SELECT appointment.id FROM Appointment appointment JOIN Professional professional ON appointment.professional.id = professional.id WHERE appointment.status LIKE 'Finalizado'")
+    List findAllProfessionalAppointmentsWithStateReprogramado();
+
+    @Query("SELECT appointment.id FROM Appointment appointment WHERE appointment.status LIKE 'Reprogramado'")
+    List findAllAppointmentsWithStateReprogramado();
 }
