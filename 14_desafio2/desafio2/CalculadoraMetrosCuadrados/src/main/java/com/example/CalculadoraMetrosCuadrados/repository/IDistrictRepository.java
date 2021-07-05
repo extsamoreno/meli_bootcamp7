a48baa080d32fda8f1c3a436b3c37b8d8952db08
natamahmud <natali.mahmud@mercadolibre.cl>
2021-07-05 17:21:11 -0400

    validaciones y test unitarios sin bugs

--- 14_desafio2/desafio2/CalculadoraMetrosCuadrados/src/main/java/com/example/CalculadoraMetrosCuadrados/repository/IDistrictRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.CalculadoraMetrosCuadrados.repository;
-
-import com.example.CalculadoraMetrosCuadrados.dto.District;
-
-public interface IDistrictRepository {
-    District getDistrictByNameAndPrice(String districtName, Double districtPrice) throws NoSuchFieldException;
-
-}
