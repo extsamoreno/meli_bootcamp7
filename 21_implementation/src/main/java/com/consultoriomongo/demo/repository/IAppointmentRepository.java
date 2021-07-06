70e1688adcf5f7a814f9ca9640e0a12f0839e2ce
nian-meli <nicoll.mejia@mercadolibre.com.co>
2021-07-05 20:01:19 -0500

    Find by doctor name

--- 21_implementation/src/main/java/com/consultoriomongo/demo/repository/IAppointmentRepository.java
+++ 21_implementation/src/main/java/com/consultoriomongo/demo/repository/IAppointmentRepository.java
@@ -4,6 +4,7 @@ package com.consultoriomongo.demo.repository;
 import com.consultoriomongo.demo.model.Appointment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +12,9 @@ import java.util.List;
 @Repository
 public interface IAppointmentRepository extends MongoRepository<Appointment, String>{
 
-    @Query("'doctor.name' : 'Doctor1'")
+    @Query("{'doctor.name' : ?0}")
     List<Appointment> findAppointmentByDoctorName(String name);
 
     List<Appointment> findAppointmentByStatus(String status);
+
 }
