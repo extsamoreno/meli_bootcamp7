6ecaf063c3ba40834fc346c1d2cbce662ec85e44
marcos-work <marcos.rausch@mercadolibre.com>
2021-07-22 15:27:47 -0300

    Java 3, TM

---
+++ 19_iSql/isql/src/main/java/com/example/meli/isql/IsqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.meli.isql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsqlApplication.class, args);
+	}
+
+}
---
+++ 19_iSql/isql/src/test/java/com/example/meli/isql/IsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meli.isql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
